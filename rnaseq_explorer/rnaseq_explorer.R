7ec6da2f29285738638150d157502610b3303a88
Jaclyn Smith <jacmarjorie@gmail.com>
2015-05-10 12:34:32 -0700

    multi python input, single file execution in R

--- rnaseq_explorer/rnaseq_explorer.R
+++ rnaseq_explorer/rnaseq_explorer.R
@@ -1,28 +1,53 @@
 library(corrplot)
 library(pheatmap)
+options(warn=-1)
+
+dg_corr_plot <- function(X){
+  row_order <- sort(rowSums(X^2, na.rm = TRUE), decreasing = TRUE, index.return = TRUE)$ix
+  col_order <- sort(colSums(X^2, na.rm = TRUE), decreasing = TRUE, index.return = TRUE)$ix
+  X <- X[row_order, col_order, drop = FALSE]
+  X <- X[which(rowMeans(is.na(X)) != 1),]
+  X[is.na(X)] <- 0
+  corrplot(X, tl.col = "black", tl.cex = .50, method = "circle", cl.pos = "n", col = corr_colors)
+}
+
+dg_corr_table <- function(X){
+  X <- X[which(rowMeans(is.na(X)) != 1),]
+  X <- cbind.data.frame(as.vector(matrix(rownames(X), nrow = nrow(X), ncol = ncol(X), byrow = FALSE)), as.vector(matrix(colnames(X), nrow = nrow(X), ncol = ncol(X), byrow = TRUE)), as.vector(X))
+  colnames(X) <- c("Drug", "Gene", "Correlation")
+  X[is.na(X)] <- 0
+  X <- X[sort(abs(X[,"Correlation"]), decreasing = TRUE, index.return = TRUE, na.last = NA)$ix,]
+  return (X)
+}
+
+dd_corr_plot <- function(X){
+  X[is.na(X)] <- 0
+  corrplot(X, tl.col = "black", tl.cex = .35, method = "circle", cl.pos = "n", order = "hclust", col = corr_colors)
+}
+
+dd_corr_table <- function(X){
+  X[lower.tri(X)] <- NA
+  X <- cbind.data.frame(as.vector(matrix(rownames(X), nrow = nrow(X), ncol = ncol(X), byrow = FALSE)), as.vector(matrix(colnames(X), nrow = nrow(X), ncol = ncol(X), byrow = TRUE)), as.vector(X))
+  colnames(X) <- c("Drug #1", "Drug #2", "Correlation")
+  X <- X[which(!is.na(X[, 3])), ]
+  X <- X[which(X[,1] != X[,2]),]
+  X <- X[sort(abs(X[,"Correlation"]), decreasing = TRUE, index.return = TRUE)$ix,]
+  return (X)
+}
 
 args <- commandArgs(TRUE)
-# args <- c('/home/jac/test_data/beataml/site1_raw_drug.csv',
-#              '/home/jac/test_data/beataml/site2_raw_drug.csv',
-#              '/home/jac/test_data/beataml/site3_raw_drug.csv',
-#              '/home/jac/test_data/beataml/site1_exp_counts.csv',
-#              '/home/jac/test_data/beataml/site2_exp_counts.csv',
-#              '/home/jac/test_data/beataml/site3_exp_counts.csv',
-#              'spearman', 'IC50')
-
-drug_screen_test <- read.csv("/home/jac/multisample/beat_aml_explorer/beataml_explorer/data/Processed_Drug_Data_beginning_to_10-4-2014_FINAL.csv", stringsAsFactors = FALSE, sep=",", check.names = FALSE)
 # set up data
-drug_screen1 <- read.csv(args[1], stringsAsFactors = FALSE, sep=",", check.names = FALSE)
-drug_screen2 <- read.csv(args[2], stringsAsFactors = FALSE, sep=",", check.names = FALSE)
-drug_screen3 <- read.csv(args[3], stringsAsFactors = FALSE, sep=",", check.names = FALSE)
-drug_screen_tmp <- rbind(drug_screen1, drug_screen2)
-drug_screen <- rbind(drug_screen_tmp, drug_screen3)
+drug_screen <- read.csv(args[1], stringsAsFactors = FALSE, sep=",", check.names = FALSE)
+# drug_screen2 <- read.csv(args[2], stringsAsFactors = FALSE, sep=",", check.names = FALSE)
+# drug_screen3 <- read.csv(args[3], stringsAsFactors = FALSE, sep=",", check.names = FALSE)
+# drug_screen_tmp <- rbind(drug_screen1, drug_screen2)
+# drug_screen <- rbind(drug_screen_tmp, drug_screen3)
 
-RNASeq1 <- read.csv(args[4], stringsAsFactors = FALSE, sep=",", row.names = "gene", check.names = FALSE)
-RNASeq2 <- read.csv(args[5], stringsAsFactors = FALSE, sep=",", row.names = "gene", check.names = FALSE)
-RNASeq3 <- read.csv(args[6], stringsAsFactors = FALSE, sep=",", row.names = "gene", check.names = FALSE)
-RNASeq_tmp <- cbind(RNASeq1, RNASeq2)
-RNASeq <- cbind(RNASeq_tmp, RNASeq3)
+RNASeq <- read.csv(args[2], stringsAsFactors = FALSE, sep=",", row.names = "gene", check.names = FALSE)
+# RNASeq2 <- read.csv(args[5], stringsAsFactors = FALSE, sep=",", row.names = "gene", check.names = FALSE)
+# RNASeq3 <- read.csv(args[6], stringsAsFactors = FALSE, sep=",", row.names = "gene", check.names = FALSE)
+# RNASeq_tmp <- cbind(RNASeq1, RNASeq2)
+# RNASeq <- cbind(RNASeq_tmp, RNASeq3)
 
 AUC_All <- tapply(drug_screen[,"Inhibitor Interpreted Result: Area under the curve"], list(r = drug_screen[,"Specimen: Lab ID"], c = drug_screen[,"Inhibitor Interpreted Result: Drug"]), mean)
 IC50_All <- tapply(drug_screen[,"Inhibitor Interpreted Result: IC50"], list(r = drug_screen[,"Specimen: Lab ID"], c = drug_screen[,"Inhibitor Interpreted Result: Drug"]), mean)
@@ -60,77 +85,44 @@ input_genes <- c("CTLA-4", "Galectin-9", "PD-L1", "PD-L2", "PD1", "TIM-3", "VIST
 colors <- colorRampPalette(c("#67001F", "#B2182B", "#D6604D", "#F4A582", "#FDDBC7", "#FFFFFF", "#D1E5F0", "#92C5DE", "#4393C3", "#2166AC", "#053061"))
 corr_colors <- rev(colors(201))
 
-if(args[7] == 'pearson' && args[8] == 'AUC'){
+if(args[3] == 'pearson' && args[4] == 'AUC'){
   
-  pdf("dg_corr_plot_auc_pear.pdf", width=7, height=10.5)
+  pdf(args[5], width=7, height=10.5)
   dg_corr_plot(BeatAML_DG_All$AUC_RNASeq_Pearson[, input_genes, drop = FALSE])
   dev.off()
   
-  pdf("dd_corr_plot_auc_pear.pdf", width=12, height=8, paper="A4r")
+  pdf(args[6], width=12, height=8, paper="A4r")
   dd_corr_plot(BeatAML_DD_All$AUC_AUC_Pearson)
   dev.off()
   
-}else if(args[7] == 'spearman' && args[8] == 'AUC'){
+}else if(args[3] == 'spearman' && args[4] == 'AUC'){
   
-  pdf("dg_corr_plot_auc_spear.pdf", width=7, height=10.5)
+  pdf(args[5], width=7, height=10.5)
   dg_corr_plot(BeatAML_DG_All$AUC_RNASeq_Spearman[, input_genes, drop = FALSE])
   dev.off()
   
-  pdf("dd_corr_plot_auc_spear.pdf", width=12, height=8, paper="A4r")
+  pdf(args[6], width=12, height=8, paper="A4r")
   dd_corr_plot(BeatAML_DD_All$AUC_AUC_Spearman)
   dev.off()
   
-}else if(args[7] == 'pearson' && args[8] == 'IC50'){
+}else if(args[3] == 'pearson' && args[4] == 'IC50'){
   
-  pdf("dg_corr_plot_ic50_pear.pdf", width=7, height=10.5)
+  pdf(args[5], width=7, height=10.5)
   dg_corr_plot(BeatAML_DG_All$IC50_RNASeq_Pearson[, input_genes, drop = FALSE])
   dev.off()
   
-  pdf("dd_corr_plot_ic50_pear.pdf", width=12, height=8, paper="A4r")
+  pdf(args[6], width=12, height=8, paper="A4r")
   dd_corr_plot(BeatAML_DD_All$IC50_IC50_Pearson)
   dev.off()
   
-}else if(args[7] == 'spearman' && args[8] == 'IC50'){
+}else if(args[3] == 'spearman' && args[4] == 'IC50'){
   
-  pdf("dg_corr_plot_ic50_spear.pdf", width=7, height=10.5)
+  pdf(args[5], width=7, height=10.5)
   dg_corr_plot(BeatAML_DG_All$IC50_RNASeq_Spearman[, input_genes, drop = FALSE])
   dev.off()
   
-  pdf("dd_corr_plot_ic50_spear.pdf", width=12, height=8, paper="A4r")
+  pdf(args[6], width=12, height=8, paper="A4r")
   dd_corr_plot(BeatAML_DD_All$IC50_IC50_Spearman)
   dev.off()
-}
-
-dg_corr_plot <- function(X){
-  row_order <- sort(rowSums(X^2, na.rm = TRUE), decreasing = TRUE, index.return = TRUE)$ix
-  col_order <- sort(colSums(X^2, na.rm = TRUE), decreasing = TRUE, index.return = TRUE)$ix
-  X <- X[row_order, col_order, drop = FALSE]
-  X <- X[which(rowMeans(is.na(X)) != 1),]
-  X[is.na(X)] <- 0
-  corrplot(X, tl.col = "black", tl.cex = .50, method = "circle", cl.pos = "n", col = corr_colors)
-}
-
-dg_corr_table <- function(X){
-  X <- X[which(rowMeans(is.na(X)) != 1),]
-  X <- cbind.data.frame(as.vector(matrix(rownames(X), nrow = nrow(X), ncol = ncol(X), byrow = FALSE)), as.vector(matrix(colnames(X), nrow = nrow(X), ncol = ncol(X), byrow = TRUE)), as.vector(X))
-  colnames(X) <- c("Drug", "Gene", "Correlation")
-  X[is.na(X)] <- 0
-  X <- X[sort(abs(X[,"Correlation"]), decreasing = TRUE, index.return = TRUE, na.last = NA)$ix,]
-  return (X)
-}
-
-dd_corr_plot <- function(X){
-  X[is.na(X)] <- 0
-  corrplot(X, tl.col = "black", tl.cex = .35, method = "circle", cl.pos = "n", order = "hclust", col = corr_colors)
-}
-
-dd_corr_table <- function(X){
-  X[lower.tri(X)] <- NA
-  X <- cbind.data.frame(as.vector(matrix(rownames(X), nrow = nrow(X), ncol = ncol(X), byrow = FALSE)), as.vector(matrix(colnames(X), nrow = nrow(X), ncol = ncol(X), byrow = TRUE)), as.vector(X))
-  colnames(X) <- c("Drug #1", "Drug #2", "Correlation")
-  X <- X[which(!is.na(X[, 3])), ]
-  X <- X[which(X[,1] != X[,2]),]
-  X <- X[sort(abs(X[,"Correlation"]), decreasing = TRUE, index.return = TRUE)$ix,]
-  return (X)
 }
 
--- rnaseq_explorer/rnaseq_explorer.py
+++ rnaseq_explorer/rnaseq_explorer.py
@@ -1,65 +1,72 @@
 # wrapper for RNASeq Explorer
-
+import os
 from argparse import ArgumentParser
 import pandas as pd
 import subprocess
 import logging
-import rpy2.robjects as robjects
-
-# functions
-r = robjects.r
-tapply = r.tapply
-mean = r.mean
 
 def explorer_argparse():
 	
 	parser = ArgumentParser(description='RNASeq Explorer: Gene Expression vs Drug Resistance')
 	parser.add_argument("-e", "--exp_files", dest = "exp_files", nargs = "+", type=str, required=True, default=None, help="List of gene expression files (raw counts, fpkm)")
 	parser.add_argument("-d", "--drug_files", dest = "drug_files", nargs = "+", type=str, required=True, default=None, help="List of drug response files")
-	
+	parser.add_argument("-a", "--algorithm", dest = "algorithm", required=True, type=str, default=None, help="Correlation Algorithm")
+	parser.add_argument("-s", "--sensitivity", dest = "sensitivity", required=True, type=str, default=None, help="Sensitivity")
+	parser.add_argument("-o1", "--output1", dest = "output1", required=True, type=str, default=None, help="Label Output File One")
+	parser.add_argument("-o2", "--output2", dest = "output2", required=True, type=str, default=None, help="Label Output File Two")
+
 	args = parser.parse_args()
 	
 	return args
 
-def read_exp(file_list):
-	master = robjects.DataFrame({})
+# read column style expression
+def consolidate_exp(file_list, workdir):
+	new_ = []
+	frame = pd.DataFrame()
 	for f in file_list:
-		if len(master) == 0:
-			master = robjects.DataFrame.from_csvfile(f, sep=",", header = True)
-		else:
-			df = robjects.DataFrame.from_csvfile(f, sep=",", header = True)
-			sub = df.rx(robjects.IntVector(range(2,len(df)+1)))
-			master = robjects.DataFrame.cbind(master, sub)
-	return master
+		with open(f) as this_file:
+			df = pd.read_csv(this_file)
+			if len(new_) == 0:
+				new_.append(df)
+			else:
+				del df['gene']
+				new_.append(df)
+	frame = pd.concat(new_, axis=1)
+	frame.to_csv(workdir+"/exp.csv", mode='w', index=False)
 
-def read_drug(file_list):
-	master = robjects.DataFrame({})
+# read row style expression
+def consolidate_drug(file_list, workdir):
+	new_ = []
+	new_f = open(workdir+"/drug.csv", "w")
+	new_f.write("Patient: Patient ID,Specimen: Lab ID,Heme Malignancy: Diagnosis,Heme Malignancy: Specific Diagnosis,Inhibitor Panel Run: Inhibitor Panel,Inhibitor Panel Run: Run Type,Inhibitor Interpreted Result: Drug,Inhibitor Interpreted Result: Replicant,Inhibitor Interpreted Result: IC10,Inhibitor Interpreted Result: IC25,Inhibitor Interpreted Result: IC50,Inhibitor Interpreted Result: IC75,Inhibitor Interpreted Result: IC90,Inhibitor Interpreted Result: Area under the curve,Inhibitor Interpreted Result: Model Curve")
 	for f in file_list:
-		if len(master) == 0:
-			master = robjects.DataFrame.from_csvfile(f, sep=",", header = True)
-		else:
-			df = robjects.DataFrame.from_csvfile(f, sep = ",", header = True)
-			master = robjects.DataFrame.rbind(master, df)
-	return master
-#disease, sensitivity, function
-def drug_gene_correlation(drug_screen):
-	auc_c = drug_screen.rx(14)
-	auc_l_d = robjects.Vector()
-	
+		with open(f) as this_file:
+			for line in this_file:
+				if 'Lab ID' not in line:
+					new_f.write(line)
+	new_f.close()
 
-def prepare_data():
-	cmd = 'R -e "shiny::runApp(port=8002)"'
+def cmd_builder(arg_dict, workdir):
+	consolidate_exp(arg_dict['exp_files'], workdir)
+	consolidate_drug(arg_dict['drug_files'], workdir)
+	exp = workdir + "/exp.csv"
+	drug = workdir + "/drug.csv"
+	return " ".join([drug,
+		exp,
+		arg_dict['algorithm'],
+		arg_dict['sensitivity'],
+		arg_dict['output1'],
+		arg_dict['output2']])
+
+def cmd_runner(args, workdir):
+	cmd = "Rscript " + workdir + "/rnaseq_explorer.R " + args
 	logging.info("RUNNING: %s" % (cmd))
-	print 'running', cmd
 	p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	stdout, stderr = p.communicate()
-	print stdout
-	if len(stderr):
-		print stderr
 	return p.returncode
 
 if __name__ == "__main__":
 	args = explorer_argparse()
-	RNASeq = read_exp(args.exp_files)
-	drug = read_drug(args.drug_files)
-	drug_gene_correlation(drug)
+	wrkdir = os.path.dirname(os.path.realpath(__file__))
+	cmd_args = cmd_builder(vars(args), wrkdir)
+	cmd_runner(cmd_args, wrkdir)
