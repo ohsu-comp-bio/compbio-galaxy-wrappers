00f7b16adbfd2c9207f2856ea95135a3c772b514
Jaclyn Smith <jacmarjorie@gmail.com>
2015-05-18 20:32:39 -0700

    fastqc

--- multi_fastqc/multi_fastqc.py
+++ multi_fastqc/multi_fastqc.py
@@ -38,12 +38,14 @@ def get_modules(sing_file):
                 #file_dict[this_mod] = file_dict[this_mod] + line + '\n'
     return file_dict
 
-def plot_per_seq_quality(in_files):
-    with PdfPages('plot_per_seq_quality.pdf') as pdf:
+def plot_per_seq_quality(in_files, output):
+    with PdfPages(output) as pdf:
         for input_f in in_files:
+            list_ = input_f.split('/')
+            leg_name = list_[len(list_)-1][:-4]
             f_dict = get_modules(input_f)
             arr_ = np.array(f_dict['Per sequence quality scores'])
-            plt.plot(arr_[:,0], arr_[:,1], label=input_f)
+            plt.plot(arr_[:,0], arr_[:,1], label=leg_name)
         plt.legend(loc='upper left')
         plt.ylabel('Count')
         plt.xlabel('Mean Sequence Quality')
@@ -51,27 +53,6 @@ def plot_per_seq_quality(in_files):
         pdf.savefig()
         plt.close()
 
-
-def cmd_builder(arg_dict, workdir):
-    consolidate_exp(arg_dict['exp_files'], workdir)
-    consolidate_drug(arg_dict['drug_files'], workdir)
-    exp = workdir + "/exp.csv"
-    drug = workdir + "/drug.csv"
-    return " ".join([drug,
-        exp,
-        arg_dict['algorithm'],
-        arg_dict['sensitivity'],
-        arg_dict['output1'],
-        arg_dict['output2']])
-
-def cmd_runner(args, workdir):
-    cmd = "Rscript " + workdir + "/rnaseq_explorer.R " + args
-    logging.info("RUNNING: %s" % (cmd))
-    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, stderr = p.communicate()
-    return p.returncode
-
 if __name__ == "__main__":
     args = explorer_argparse()
-    wrkdir = os.path.dirname(os.path.realpath(__file__))
-    plot_per_seq_quality(vars(args)['input'])
+    plot_per_seq_quality(vars(args)['input'], vars(args)['output1'])
