deb4eeda1cd006602cd5b7f5e79c7b27ef7a7e67
letaw <letaw@ohsu.edu>
2019-01-28 21:10:22 -0800

    Update TMB calculation to include VAF cutoff argument

--- calc_tmb/calc_tmb.py
+++ calc_tmb/calc_tmb.py
@@ -4,7 +4,7 @@ import argparse
 import json
 import vcf
 
-VERSION = '0.1.1'
+VERSION = '0.2.0'
 
 def supply_args():
     """
@@ -15,7 +15,8 @@ def supply_args():
     parser.add_argument('--infile', help='Input VCF')
     parser.add_argument('--outfile', help='Output JSON')
     parser.add_argument('--gnomad_af', default=0.00001, type=float, help='GNOMAD VAF to filter list upon.  Values greater than this will not be utilized.')
-    parser.add_argument('--m2_tlod', default=20.0, type=float, help='M2 TLOD score to filter list upon.  Values less than this will not be utilized.')
+    parser.add_argument('--m2_tlod', default=40.0, type=float, help='M2 TLOD score to filter list upon.  Values less than this will not be utilized.')
+    parser.add_argument('--min_ab', default=0.05, type=float, help='Variant allele frequency (balance) to filter list upon.  Values less than this will not be utilized.')
     parser.add_argument('--seq_space', default=0.61, type=float, help='Total genomic size of sequence targets, in Mb.')
     parser.add_argument('--version', action='version', version='%(prog)s ' + VERSION)
     args = parser.parse_args()
@@ -27,11 +28,12 @@ class VcfRec(object):
         self.rec = rec
         self.chrom = str(rec.CHROM)
         self.coord = str(rec.POS)
+        self.ab = float(rec.samples[0]['AF'])
 
         try:
             self.gnomad = float(rec.INFO['gnomad.AF'][0])
         except:
-            self.gnomad = 0.0
+            self.gnomad = None
 
         try:
             self.snpeff = rec.INFO['ANN']
@@ -50,7 +52,8 @@ def write_out(filename, tmb, mut_cnt):
     :return:
     """
     outfile = open(filename, 'w')
-    out_metric = {'tmb': tmb, 'tmb_mut_cnt': mut_cnt}
+    # out_metric = {'tmb': tmb, 'tmb_mut_cnt': mut_cnt}
+    out_metric = {'tmb': tmb}
     json.dump(out_metric, outfile)
     outfile.close()
 
@@ -62,11 +65,15 @@ def main():
     for record in vcf_reader:
         if record.is_snp:
             entry = VcfRec(record)
-            if entry.gnomad < args.gnomad_af:
-                if "missense_variant" in entry.snpeff[0]:
-                    if entry.tlod > args.m2_tlod:
-                        tmb_cnt += 1
-
+            if entry.gnomad:
+                if entry.gnomad < args.gnomad_af:
+                    if "missense_variant" in entry.snpeff[0]:
+                        if entry.tlod > args.m2_tlod:
+                            if entry.ab > args.min_ab:
+                                tmb_cnt += 1
+                                print(record)
+                                print(record.INFO)
+                                print(record.samples)
     tmb_calc = '{:.1f}'.format(tmb_cnt / args.seq_space)
     write_out(args.outfile, tmb_calc, int(tmb_cnt))
 
