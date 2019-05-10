5f7cf98fc3511f406d06e06b59cbdc5478ba9d2f
NEleanor <43549870+NEleanor@users.noreply.github.com>
2019-05-10 13:37:08 -0700

    seq dict handling
    added sequence dictionary handling and cleaned up comments.

--- CNV_plotter/gtrellis_CNV_NT.R
+++ CNV_plotter/gtrellis_CNV_NT.R
@@ -9,118 +9,96 @@ if ( !gtrellisPackageExists ) {
   
 }
 
-# tidyversePackageExists <- require ("tidyverse")
-
-# if ( !tidyversePackageExists ) {
-  # install.packages ("tidyverse")
-  # library ("tidyverse")
- # }
 #########################
 
 #Get the inputs
+#send command line arguments to args
 args <- commandArgs(TRUE)
+#get the tumor percent, as a value from 0-100
 tumorPercent <- as.double(args[1])
+#The name of the file containing the read count data
 countFile <- args[2]
+# the name of the file containing the segment data
 segmentFile <- args[3]
+#What to name the sample in teh plots
 SampleName <- args[4]
+#the file containing the relevant genes and thier locations, for coloring the dots
 geneFile <- args[5]
+#the file containing the chromosome lengths. can be either basic CHR, START, END or a sequence dictionary, for placing the pink lines
 chromFile <- args[6]
+#the upper and lower bounds of normal to place the pink range lines
 upper <- as.double(args[7])
 lower <- as.double(args[8])
 
 #turn tumor percent into a fraction
-
 tumorPercent = tumorPercent/100
 
-# read file that contains hg19 chromosome BED, along with max and min copy values values that will be plotted as lines across each plot
-# working directory is "C:/Users/beadling/Documents"
-copiesUpperLower<-read.delim(chromFile, header = TRUE, sep = "\t")
+#find out what type of chrom file was entered by taking the last four characters
+chromType = substr(chromFile, nchar(chromFile)-3, nchar(chromFile))
 
-
-#read in the files from galaxy steps 100, 111, and 115
+#if the type of file is a sequence dictionary it will end in "dict"
+if (chromType == "dict") {
+	#if using a sequence dictionary reformat it for use.
+	#read in the sequence dictionary, fill in missing values with blanks, there is no header, tab separated
+	copiesUpperLower<-read.table(chromFile, fill=TRUE, header = FALSE, sep = "\t")
+	#get the chromosome column by splitting the second column by : and taking the second part of the split
+	copiesUpperLower$CHR = data.frame(do.call('rbind', strsplit(as.character(copiesUpperLower[,2]), ":", fixed=TRUE)))$X2
+	#All the starts are 1, make teh start column
+	copiesUpperLower$START = 1
+	#Get the end cloumn usign the same method as teh chr column but splitting the third column instead.
+	copiesUpperLower$END = data.frame(do.call('rbind', strsplit(as.character(copiesUpperLower[,3]), ":", fixed=TRUE)))$X2
+	#remove all irrelevant columns
+	copiesUpperLower = copiesUpperLower[,c("CHR", "START", "END")]
+	#properly format the columns as character or numbers
+	copiesUpperLower$CHR <- lapply(copiesUpperLower$CHR, as.character)
+	copiesUpperLower$END <- lapply(copiesUpperLower$END, as.character)
+	copiesUpperLower$END <- sapply(copiesUpperLower$END, as.numeric)
+	#remove all rows that have non-chromosome contigs
+	copiesUpperLower = subset(copiesUpperLower, nchar(CHR) < 3)
+	#remove the mitochondrial contig
+	copiesUpperLower = subset(copiesUpperLower, CHR != "MT")
+	#add the required "chr" to the contig names
+	copiesUpperLower$CHR = paste0("chr", copiesUpperLower$CHR)
+} else {
+	#if using the already formatted chrom file simply read it in wiht a header. should have CHR, START and END, headers and be separated by tabs
+	copiesUpperLower<-read.delim(chromFile, header = TRUE, sep = "\t")
+}
+#read in the files for the read counts, segemnts and genefiel. 
+#read counts will create the blue dots
 countsData <- read.table(countFile, header = TRUE, sep = "\t", comment.char = "@")
+#segements create teh green lines
 segmentsData <- read.table(segmentFile, header = TRUE, sep = "\t", comment.char = "@")
+#gene intervals color the dots based on revelance
 geneIntervals <- read.table(geneFile, header = TRUE, sep = "\t", comment.char = "@")
 
-#geneIntervals <- mutate(geneIntervals, hg19Loc = substring(STRINGhg19, 4))
-#geneIntervals$hg19Loc = substr(geneIntervals$STRINGhg19, 4)
-
+#a function to get the correct number of copies based on teh tumor percent. Pass it the data sheet in question and which column has the uncorrected copy number 
 correctCopies <- function(data_sheet, log_name) {
 	#remove empty lines
 	data_sheet<-data.frame(data_sheet[complete.cases(data_sheet),])
 	#get the tumor corrected copies number
-	#data_with_neg <- mutate(data_sheet, tumorCorrectedCopies = ((2^(log_name + 1))-(2*(1-tumorPercent))/tumorPercent))
 	data_sheet$tumorCorrectedCopies = ((2^(log_name + 1))-(2*(1-tumorPercent))/tumorPercent)
 	#replace the negatives with .1
-	#data_no_neg <- mutate(data_with_neg, Tumor_Corrected_Copies_STPv3 = if_else(tumorCorrectedCopies > 0, tumorCorrectedCopies, 0.1))
 	data_sheet$Tumor_Corrected_Copies_STPv3 = (ifelse(data_sheet$tumorCorrectedCopies > 0, data_sheet$tumorCorrectedCopies, .1))
-	#data_sheet$tumorCorrectedCopies[data_sheet$tumorCorrectedCopies < 0] <- .1
-	#data_sheet$Tumor_Corrected_Copies_STPv3 = data_sheet$tumorCorrectedCopies
-	#return only the needed columns, contig to end and tumore corrected copies
-	#return(select(data_no_neg, CONTIG:END, Tumor_Corrected_Copies_STPv3))
+	#return only the needed columns, contig start end and tumore corrected copies
 	return(data_sheet[,c("CONTIG", "START", "END", "Tumor_Corrected_Copies_STPv3")])
 }
 
-#do the mutations on both data sets
+#Get the tumor corrected copy number for both the coutns and the segments
 cnvPoints <- correctCopies(countsData, countsData$LOG2_COPY_RATIO)
 cnvSegments <- correctCopies(segmentsData, segmentsData$MEAN_LOG2_COPY_RATIO)
 
 #get the stpv3_225 data from the gene intervals data
-#create a single location column
-#cnvPoints <- mutate(cnvPoints, locationStart = paste(CONTIG,START, sep=":"))
-#cnvPoints <- mutate(cnvPoints, hg19Loc = paste(locationStart,END, sep="-"))
+#create a new column with the cromosomal location in a single column for mergeing with the gene interval file
 cnvPoints$hg19Loc = paste0("chr", cnvPoints$CONTIG, ":", cnvPoints$START, "-", cnvPoints$END)
 #merge the gene interval information with the cnv points information
-#print.table(cnvPoints)
-#print.table(geneIntervals)
 cnvPoints <- merge(cnvPoints, geneIntervals)
 #remove empty lines
-#print.table(cnvPoints)
 cnvPoints<-data.frame(cnvPoints[complete.cases(cnvPoints),])
 
-#cnvPoints <- select(cnvPoints, CONTIG, START, END, STPv3_225, Tumor_Corrected_Copies_STPv3)
-cnvPoints = cnvPoints[,c("CONTIG", "START", "END", "STPv3_225", "Tumor_Corrected_Copies_STPv3")]
-#cnvSegments <- select(cnvSegments, CONTIG, START, END, Tumor_Corrected_Copies_STPv3)
-cnvSegments = cnvSegments[,c("CONTIG", "START", "END", "Tumor_Corrected_Copies_STPv3")]
-
-#cnvPoints <- mutate(cnvPoints, CONTIG = paste("chr", CONTIG, sep=''))
+#add the needed "chr" to each chromosome name in the counts dataset and teh segments dataset
 cnvPoints$CONTIG = paste0("chr", cnvPoints$CONTIG)
-#cnvSegments <- mutate(cnvSegments, CONTIG = paste("chr", CONTIG, sep=''))
 cnvSegments$CONTIG = paste0("chr", cnvSegments$CONTIG)	
 
-
-# # Make list of Excel files that contain CNV data; each file has multiple sheets
-# file.list <- list.files(path = "C:/Users/beadling/Desktop/STPv3CNV/", pattern='*_STPv3_CNV.xlsx', full.names = TRUE)
-
-# # Read Excel sheets with CNV segment data; append filename in Source column; combine data for all samples into one file
-# df.list.segments <- lapply(file.list, read_excel, sheet = "SEGMENTS")
-  # names(df.list.segments)<-file.list
-  # for (i in file.list)
-  # df.list.segments[[i]]$Source = i
-  # do.call(rbind, df.list.segments)  
-   
-# # Read Excel sheets with CNV counts data; append filename in Source column;  combine data for all samples into one file    
-# df.list.counts <- lapply(file.list, read_excel, she~/Box Sync/CNV/gene_intervals.txtet = "COUNTS")
-  # names(df.list.counts)<-file.list
-  # for (i in file.list)
-  # df.list.counts[[i]]$Source = i
-  # do.call(rbind, df.list.counts)   
-  
-# # Create SampleName, segments, and points from each sample Excel file in file.list
-# for(i in 1:length(file.list)){
- # SampleName <- basename(file.list[[i]])
- # cnvSegments<-df.list.segments[[file.list[i]]]
- # cnvPoints<-df.list.counts[[file.list[i]]]
- 
- 
-
-# Low % tumor cases may have negative tumor-corrected copy number, which generates error upon log2 transformation
-# Corrected these in Excel prior to import into R, by recoding negative tumor-corrected copy number values to positive number 0.1
-# Exclude rows that have missing values
-#cnvPoints<-data.frame(cnvPoints[complete.cases(cnvPoints),])
-#cnvSegments<-data.frame(cnvSegments[complete.cases(cnvSegments),])
- 
- 
 ## First plot of all chr in one row  
 # create png for plot1
 png(filename = "plot1.png", 
@@ -130,10 +108,8 @@ png(filename = "plot1.png",
 
 
 # plot1 has one row for all chr 
-# remove .xlsx from end of SampleName
 # ylim max is set to max point value for sample, plus add 1 for buffer to prevent points from getting squished at top
 # ylim min is fixed at -5 for all samples; may cut out failed assays, but prevents individual failed assays from making min too low
-#print.table(cnvPoints)
 gtrellis_layout(n_track = 1, 
                 title = SampleName,
                 title_fontsize = 32,
@@ -151,7 +127,6 @@ gtrellis_layout(n_track = 1,
 
 # color points differently if targets are within or outside STPv3 genes of interest
 # blue == within STPv3 gene of interest; grey == outside STPv3 gene of interest; red == neither
-# add lines across plots to demarcate 5 and 0.5 tumor-corrected copies; log2(5)=2.32; log2(0.5)= -1
 
 add_points_track(cnvPoints, log2(cnvPoints$Tumor_Corrected_Copies_STPv3),
                  pch = 16, 
@@ -160,12 +135,14 @@ add_points_track(cnvPoints, log2(cnvPoints$Tumor_Corrected_Copies_STPv3),
                                  ifelse(cnvPoints$STPv3_225 == "0", "grey", "red")))
                 ) 
 
-
+#add the green segement lines
 add_segments_track(cnvSegments, log2(cnvSegments$Tumor_Corrected_Copies_STPv3),track=current_track(),
                    gp = gpar(col = "green",
                              lwd = 5)
                    ) 
-                   
+   
+# add lines across plots to demarcate 5 and 0.5 tumor-corrected copies; log2(5)=2.32; log2(0.5)= -1
+# the location of the lines at 5 and .5 are now moveable with the upper and lower arguments.              
 add_segments_track(copiesUpperLower, log2(upper),track=current_track(),
                 gp = gpar(col = "pink",
                 lty = 2,
@@ -177,17 +154,6 @@ add_segments_track(copiesUpperLower, log2(lower),track=current_track(),
                 lty = 2,
                 lwd = 1)
                 )                   
-# add_segments_track(copiesMaxMin, log2(copiesUpperLower$UPPER),track=current_track(),
-                # gp = gpar(col = "pink",
-                # lty = 2,
-                # lwd = 1)
-                # )
-
-# add_segments_track(copiesMaxMin, log2(copiesUpperLower$LOWER),track=current_track(),
-                   # gp = gpar(col = "pink",
-                             # lty = 2,
-                             # lwd = 1)
-                  # )
 
 # complete png export plot1
 dev.off()
@@ -222,7 +188,7 @@ gtrellis_layout(n_track = 1,
 
 # color points differently if targets are within or outside STPv3 genes of interest
 # blue == within STPv3 gene of interest; grey == outside STPv3 gene of interest; red == neither
-# add lines across plots to demarcate 5 and 0.5 tumor-corrected copies; log2(5)=2.32; log2(0.5)= -1
+
 add_points_track(cnvPoints, log2(cnvPoints$Tumor_Corrected_Copies_STPv3),
                  pch = 16, 
                  size = unit(1.5, "mm"),
@@ -234,7 +200,8 @@ add_segments_track(cnvSegments, log2(cnvSegments$Tumor_Corrected_Copies_STPv3),t
                    gp = gpar(col = "green",
                              lwd = 2)
                    ) 
-                   
+# add lines across plots to demarcate 5 and 0.5 tumor-corrected copies; log2(5)=2.32; log2(0.5)= -1
+# the location of the lines at 5 and .5 are now moveable with the upper and lower arguments.            
 add_segments_track(copiesUpperLower, log2(upper),track=current_track(),
                 gp = gpar(col = "pink",
                 lty = 2,
@@ -246,18 +213,6 @@ add_segments_track(copiesUpperLower, log2(lower),track=current_track(),
                 lty = 2,
                 lwd = 1)
                 )
-
-# add_segments_track(copiesMaxMin, log2(copiesUpperLower$UPPER),track=current_track(),
-                   # gp = gpar(col = "pink",
-                             # lty = 2,
-                             # lwd = 0.1)
-                  # )
-
-# add_segments_track(copiesMaxMin, log2(copiesUpperLower$LOWER),track=current_track(),
-                   # gp = gpar(col = "pink",
-                             # lty = 2,
-                             # lwd = 0.1)
-                             # )
 
 # complete png export plot2
 dev.off()
