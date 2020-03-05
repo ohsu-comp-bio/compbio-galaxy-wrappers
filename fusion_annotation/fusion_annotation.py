d29a3d3be2fbf4fa93c63a88010e64552ac22f58
letaw <letaw@ohsu.edu>
2020-03-05 14:09:01 -0800

    regex change

--- fusion_annotation/fusion_annotation.py
+++ fusion_annotation/fusion_annotation.py
@@ -270,7 +270,7 @@ def main():
                        "TRNI", "TRNK", "TRNL1", "TRNL2", "TRNM", "TRNN", "TRNP",
                        "TRNQ", "TRNR", "TRNS1", "TRNS2", "TRNT", "TRNV", "TRNW", "TRNY",
                        "IGH-@-ext"]
-        regex_filt = [r'A[LC][0-9]{6}\.[0-9]{1}', r'RP11-[0-9]{3}[A-Z]{1}[0-9]+\.[0-9]{1}']
+        regex_filt = [r'A[LC][0-9]{6}\.[0-9]{1}', r'RP[0-9]+-[0-9]{3}[A-Z]{1}[0-9]+\.[0-9]{1}']
 
     else:
         hard_filter = None
