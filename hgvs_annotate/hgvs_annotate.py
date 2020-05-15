3264db4a51ba1abdfc37cb6bf6f2b8ec0c4d1c41
letaw <jhl667@yahoo.com>
2020-05-15 09:20:24 -0700

    getting multiprocessing up and going

--- hgvs_annotate/hgvs_annotate.py
+++ hgvs_annotate/hgvs_annotate.py
@@ -7,6 +7,7 @@
 # CODED BY: John Letaw
 
 import argparse
+import multiprocessing as mp
 import vcfpy
 import hgvs.assemblymapper
 import hgvs.dataproviders.uta
@@ -46,49 +47,153 @@ def supply_args():
     return args
 
 
-def correct_indel_coords(pos, ref, alt):
+
+
+class VcfRec:
     """
-    Using a VCF position, create coords that are compatible with HGVS nomenclature.
-    Since we are already determining at this stage whether the event is an ins or del, also
-    include the ins or del strings in the result.
-    substitution event -> ac:g.[pos][ref]>[alt]
-    :return:
+    Information corresponding to a single VCF entry.
     """
-    lref = len(ref)
-    lalt = len(alt)
-    if lref == 1 and lalt == 1:
-        # Substitution case
-        change = '>'.join([ref, alt])
-        new_pos = str(pos) + change
-        return new_pos
-    elif lref == lalt:
-        # Multi-nucleotide substitution case
-        # NG_012232.1: g.12_13delinsTG
-        new_start = str(pos)
-        new_end = str(int(pos) + lref - 1)
-        new_pos = '_'.join([new_start, new_end]) + 'delins' + alt
-        return new_pos
-    elif lref > lalt:
-        # Deletion case
-        shift = lref - lalt
-        if shift == 1:
-            new_pos = str(int(pos) + 1) + 'del'
+    def __init__(self, rec, hp):
+        self.hp = hp
+        self.chrom = CHROM_MAP[rec.CHROM]
+        self.pos = rec.POS
+        self.ref = rec.REF
+        self.alt = [x.value for x in rec.ALT]
+        self.hgvs_g = self._prep_hgvs()
+        self.var_g = self._create_var_g()
+
+    def _create_var_g(self):
+        var_g = []
+        for entry in self.hgvs_g:
+            var_g.append(self.hp.parse_hgvs_variant(entry))
+        return var_g
+
+    def _prep_hgvs(self):
+        """
+
+        :return:
+        """
+        hgvs_g = []
+        for entry in self.alt:
+            pos_part = self.correct_indel_coords(self.pos, self.ref, str(entry))
+            new_hgvs = self.chrom + ':g.' + pos_part
+            hgvs_g.append(new_hgvs)
+        return hgvs_g
+
+    @staticmethod
+    def correct_indel_coords(pos, ref, alt):
+        """
+        Using a VCF position, create coords that are compatible with HGVS nomenclature.
+        Since we are already determining at this stage whether the event is an ins or del, also
+        include the ins or del strings in the result.
+        substitution event -> ac:g.[pos][ref]>[alt]
+        :return:
+        """
+        lref = len(ref)
+        lalt = len(alt)
+        if lref == 1 and lalt == 1:
+            # Substitution case
+            change = '>'.join([ref, alt])
+            new_pos = str(pos) + change
+            return new_pos
+        elif lref == lalt:
+            # Multi-nucleotide substitution case
+            # NG_012232.1: g.12_13delinsTG
+            new_start = str(pos)
+            new_end = str(int(pos) + lref - 1)
+            new_pos = '_'.join([new_start, new_end]) + 'delins' + alt
+            return new_pos
+        elif lref > lalt:
+            # Deletion case
+            shift = lref - lalt
+            if shift == 1:
+                new_pos = str(int(pos) + 1) + 'del'
+                return new_pos
+            else:
+                new_start = str(int(pos) + 1)
+                new_end = str(int(pos) + shift)
+                new_pos = '_'.join([new_start, new_end]) + 'del'
+                return new_pos
+        elif lalt > lref:
+            # Insertion case
+            new_start = str(pos)
+            new_end = str(int(pos) + 1)
+            new_pos = '_'.join([new_start, new_end]) + 'ins' + alt[1:]
             return new_pos
         else:
-            new_start = str(int(pos) + 1)
-            new_end = str(int(pos) + shift)
-            new_pos = '_'.join([new_start, new_end]) + 'del'
-            return new_pos
-    elif lalt > lref:
-        # Insertion case
-        new_start = str(pos)
-        new_end = str(int(pos) + 1)
-        new_pos = '_'.join([new_start, new_end]) + 'ins' + alt[1:]
-        return new_pos
-    else:
-        # OTHER case
-        print("Change type not supported: " + pos + ':' + ref + '>' + alt + '\n')
+            # OTHER case
+            print("Change type not supported: " + pos + ':' + ref + '>' + alt + '\n')
 
+
+class VcfReader:
+    """
+
+    """
+    def __init__(self, filename, hp):
+        self.hp = hp
+        self.vcf_reader = vcfpy.Reader.from_path(filename)
+        self.vcf_reader.header.add_info_line(vcfpy.OrderedDict([('ID', 'HGVS_G'),
+                                                           ('Number', '.'),
+                                                           ('Type', 'String'),
+                                                           ('Description', 'HGVS genomic reference, as produced by pyhgvs.')]))
+        self.vcf_reader.header.add_info_line(vcfpy.OrderedDict([('ID', 'HGVS_C'),
+                                                           ('Number', '.'),
+                                                           ('Type', 'String'),
+                                                           ('Description', 'HGVS coding reference, as produced by pyhgvs.')]))
+        self.vcf_reader.header.add_info_line(vcfpy.OrderedDict([('ID', 'HGVS_P'),
+                                                           ('Number', '.'),
+                                                           ('Type', 'String'),
+                                                           ('Description', 'HGVS protein reference, as produced by pyhgvs.')]))
+        self.my_vrnts = self._create_rec()
+
+
+    def _create_rec(self):
+        """
+        """
+        my_vrnts = []
+        for vrnt in self.vcf_reader:
+            my_vrnts.append(VcfRec(vrnt, self.hp))
+        return my_vrnts
+
+
+        # for entry in alt:
+        #     var_g = hp.parse_hgvs_variant(new_hgvs)
+        #     # This is how you find out which transcripts are available.  Might as well provide HGVS for all of them.
+        #
+        #     if 'HGVS_G' not in record.INFO:
+        #         record.INFO['HGVS_G'] = [str(var_g)]
+        #     else:
+        #         record.INFO['HGVS_G'].append(str(var_g))
+        #
+        #     tx_list = hdp.get_tx_for_region(str(var_g.ac), 'splign',
+        #                                     str(var_g.posedit.pos.start), str(var_g.posedit.pos.end))
+        #     for tx in tx_list:
+        #         try:
+        #             var_c = vm.g_to_c(var_g, str(tx[0]))
+        #             if 'HGVS_C' not in record.INFO:
+        #                 record.INFO['HGVS_C'] = [str(var_c)]
+        #             else:
+        #                 record.INFO['HGVS_C'].append(str(var_c))
+        #
+        #             var_p = vm.c_to_p(var_c)
+        #             if 'HGVS_P' not in record.INFO:
+        #                 record.INFO['HGVS_P'] = [str(var_p)]
+        #             else:
+        #                 record.INFO['HGVS_P'].append(str(var_p))
+        #
+        #         except:
+        #             print("Transcript probably non-coding, ignore.")
+        #
+        # return record
+
+def get_txs(my_vcf, hdp):
+    """
+    
+    :return: 
+    """
+    for rec in my_vcf:
+        for var_g in rec.var_g:
+            print(hdp.get_tx_for_region(str(var_g.ac), 'splign', str(var_g.posedit.pos.start), str(var_g.posedit.pos.end)))
 
 def main():
 
@@ -98,90 +203,42 @@ def main():
     hdp = hgvs.dataproviders.uta.connect()
     vm = hgvs.assemblymapper.AssemblyMapper(hdp, assembly_name="GRCh37", alt_aln_method='splign')
     hp = hgvs.parser.Parser()
-
-    if args.chrom:
-        chrom = CHROM_MAP[args.chrom]
-        pos = args.pos
-        ref = args.ref
-        alt = args.alt
-        pos_part = correct_indel_coords(pos, ref, alt)
-        new_hgvs = chrom + ':g.' + pos_part
-        var_g = hp.parse_hgvs_variant(new_hgvs)
-        # This is how you find out which transcripts are available.  Might as well provide HGVS for all of them.
-
-        tx_list = hdp.get_tx_for_region(str(var_g.ac), 'splign',
-                                        str(var_g.posedit.pos.start), str(var_g.posedit.pos.end))
-        for entry in tx_list:
-            print(entry[0])
-            print(var_g)
-            try:
-                var_c = vm.g_to_c(var_g, entry[0])
-                print(str(var_c))
-                var_p = vm.c_to_p(var_c)
-                print(str(var_p))
-            except:
-                print("NON-CODING LIKELY.")
+    pool = mp.Pool(16)
 
     if args.input_vcf:
-        vcf_reader = vcfpy.Reader.from_path(args.input_vcf)
-        vcf_reader.header.add_info_line(vcfpy.OrderedDict([('ID', 'HGVS_G'),
-                                                           ('Number', '.'),
-                                                           ('Type', 'String'),
-                                                           ('Description', 'HGVS genomic reference, as produced by pyhgvs.')]))
-        vcf_reader.header.add_info_line(vcfpy.OrderedDict([('ID', 'HGVS_C'),
-                                                           ('Number', '.'),
-                                                           ('Type', 'String'),
-                                                           ('Description', 'HGVS coding reference, as produced by pyhgvs.')]))
-        vcf_reader.header.add_info_line(vcfpy.OrderedDict([('ID', 'HGVS_P'),
-                                                           ('Number', '.'),
-                                                           ('Type', 'String'),
-                                                           ('Description', 'HGVS protein reference, as produced by pyhgvs.')]))
-        vcf_writer = vcfpy.Writer.from_path(args.output_vcf, vcf_reader.header)
+        # vcf_writer = vcfpy.Writer.from_path(args.output_vcf, vcf_reader.header)
+        my_vcf = VcfReader(args.input_vcf, hp).my_vrnts
+        results = pool.starmap(get_txs, [(x for x in my_vcf), hdp])
 
-        i = 0
+        # for entry in results:
+        #     print(entry)
+        #     raise Exception("ASHIFS")
 
-        for record in vcf_reader:
-            try:
-                chrom = CHROM_MAP[record.CHROM]
-            except:
-                continue
-            pos = record.POS
-            ref = record.REF
-            alt = [x.value for x in record.ALT]
-            for entry in alt:
-                pos_part = correct_indel_coords(pos, ref, str(entry))
-                new_hgvs = chrom + ':g.' + pos_part
-                var_g = hp.parse_hgvs_variant(new_hgvs)
-                # This is how you find out which transcripts are available.  Might as well provide HGVS for all of them.
+        # my_hgvs = HgvsVcfWriter(args.input_vcf, args.output_vcf, hp, hdp, vm)
+        # results = create_hgvs(my_hgvs)
 
-                if 'HGVS_G' not in record.INFO:
-                    record.INFO['HGVS_G'] = [str(var_g)]
-                else:
-                    record.INFO['HGVS_G'].append(str(var_g))
-
-                tx_list = hdp.get_tx_for_region(str(var_g.ac), 'splign',
-                                                str(var_g.posedit.pos.start), str(var_g.posedit.pos.end))
-                for tx in tx_list:
-                    try:
-                        var_c = vm.g_to_c(var_g, str(tx[0]))
-                        if 'HGVS_C' not in record.INFO:
-                            record.INFO['HGVS_C'] = [str(var_c)]
-                        else:
-                            record.INFO['HGVS_C'].append(str(var_c))
-
-                        var_p = vm.c_to_p(var_c)
-                        if 'HGVS_P' not in record.INFO:
-                            record.INFO['HGVS_P'] = [str(var_p)]
-                        else:
-                            record.INFO['HGVS_P'].append(str(var_p))
-
-                    except:
-                        print("Transcript probably non-coding, ignore.")
-
-            vcf_writer.write_record(record)
-            if i % 10000 == 0:
-                print(i)
-            i += 1
+    # if args.chrom:
+    #     chrom = CHROM_MAP[args.chrom]
+    #     pos = args.pos
+    #     ref = args.ref
+    #     alt = args.alt
+    #     pos_part = correct_indel_coords(pos, ref, alt)
+    #     new_hgvs = chrom + ':g.' + pos_part
+    #     var_g = hp.parse_hgvs_variant(new_hgvs)
+    #     # This is how you find out which transcripts are available.  Might as well provide HGVS for all of them.
+    #
+    #     tx_list = hdp.get_tx_for_region(str(var_g.ac), 'splign',
+    #                                     str(var_g.posedit.pos.start), str(var_g.posedit.pos.end))
+    #     for entry in tx_list:
+    #         print(entry[0])
+    #         print(var_g)
+    #         try:
+    #             var_c = vm.g_to_c(var_g, entry[0])
+    #             print(str(var_c))
+    #             var_p = vm.c_to_p(var_c)
+    #             print(str(var_p))
+    #         except:
+    #             print("NON-CODING LIKELY.")
 
 
 if __name__ == "__main__":
