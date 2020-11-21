a2bb8f37073aee30b02d60fd221497129a5e542d
letaw <jhl667@yahoo.com>
2020-11-20 17:10:41 -0800

    now python3 compatible

--- sample_metrics/sample_metrics_rna.py
+++ sample_metrics/sample_metrics_rna.py
@@ -1,11 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # DESCRIPTION: Create sample level metrics to be passed to the CGD for RNA.  Metrics
 #  are passed as a json dump.
 # usage: sample_metrics.py -h
-# by: Janice
-# todo:
-#   optional arguments when tpm is empty.
+#
+#   DO: optional arguments when tpm is empty.
 
 
 from __future__ import print_function
@@ -15,12 +14,12 @@ import json
 import numpy
 from pybedtools import BedTool
 
-VERSION = '0.2.2'
+VERSION = '0.3.0'
 
 
 def main():
     args = supply_args()
-    target_genes = targetbed_GTF(args.filtered_gtf)
+    target_genes = targetbed_gtf(args.filtered_gtf)
     sumrz_cnts, hk_counts = summarize_starcounts(args.counts, target_genes, args.hk_file)
 
     if args.tpm:
@@ -30,13 +29,10 @@ def main():
 
 
 def supply_args():
-    """
-
-    """
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('--counts', help='STAR_output_reads_per_gene')
     parser.add_argument('--tpm', help="tpmfile converted from counts, ENSG ids only")
-    parser.add_argument('--filtered_gtf', help = 'ref_GRCh37.p13.truseq-rna-exome-targets.gtf')
+    parser.add_argument('--filtered_gtf', help='ref_GRCh37.p13.truseq-rna-exome-targets.gtf')
     parser.add_argument('--hk_file', help='List of housekeeping genes to output')
     parser.add_argument('--out', help='Output file in human readable text format.')
     parser.add_argument('--outjson', help='json output for CGD')
@@ -45,30 +41,21 @@ def supply_args():
     return args
 
 
-
-def targetbed_GTF(filtered_gtf):
+def targetbed_gtf(filtered_gtf):
     """
     :return list of HGVS for genes only in the target file
     """
-
     gtf = BedTool(filtered_gtf)
     targetgenes = set()
-    #gtf.filter(lambda x: x[2] == 'gene')
     for feature in gtf:
         attrib = str(feature[8].strip().split(';')[2])
         targetgenes.add(attrib.split('"')[1])
-
-
-    return(targetgenes)
+    return targetgenes
 
 
 def get_hkgenes(filename_hk, target_genes, dict_of_abundance):
     """
     Housekeeping genes, filter for those genes are in the target file
-    :param list of housekeeping genes
-    :param target_genes (list) HGVS name
-    :param dict_of_abundance (dict)  of gene names: counts/tpm
-    :return: dict_of_hkgenes (dict)
     """
     hk_genes = {}
 
@@ -79,14 +66,12 @@ def get_hkgenes(filename_hk, target_genes, dict_of_abundance):
                 hk_genes[geneid[0]] = [geneid[0], dict_of_abundance[geneid[0]]]
             else:
                 print(geneid[0] + ": ", "Check gene id format (Hugo/ENSG) or gene is not in target intervals.")
-    return(hk_genes)
+    return hk_genes
+
 
 def summarize_starcounts(infile, target_genes, filename_hk):
     """
     Summarize the reads.per.gene from STAR to TPM for table
-    :param infile reads.per.gene
-    :param list of target_genes
-    :return sumrz_counts (dict)
     """
     rev_counts = {}
     other = {}
@@ -102,45 +87,43 @@ def summarize_starcounts(infile, target_genes, filename_hk):
 
     np_counts = numpy.asarray(target_counts, dtype=numpy.float64)
     sumrz_counts["COUNT_0"] = len(np_counts[np_counts >= 0])
-    for i in range(0,6):
+    for i in range(0, 6):
         idx = pow(10, i)
         sumrz_counts["COUNT_" + str(idx)] = len(np_counts[np_counts >= idx])
 
     hk_counts = get_hkgenes(filename_hk, target_genes, rev_counts)
-    return(sumrz_counts, hk_counts)
+    return sumrz_counts, hk_counts
 
 
 def summarize_tpm(filename_tpm, target_genes, filename_hk):
     """
     Summarize the converted TPM (calc_norm.py) reads.per.gene from STAR to TPM for table
     :param filename_tpm (string) filename of tpm translated output
-    :param sumrz_counts (dict)
     :param target_genes (list)  target HUGO gene ids
     :param filename_hk (string) filename of housekeeping gene list
     :return sumrz_tpm (dict)
     :return hk_genes (list of list)
     """
     tpm = {}
-    sumrz_tpm = {}  #tuple, ordered
+    # tuple, ordered
+    sumrz_tpm = {}
     with open(filename_tpm, 'rU') as fh_tpm:
         for lines in fh_tpm:
             line = lines.rstrip('\n').split('\t')
             if line[0] in target_genes:
                 tpm[line[0]] = line[1]
 
-
-    #Summarize TPM
-    np_tpm = numpy.asarray(tpm.values(), dtype=numpy.float64)
+    # Summarize TPM
+    np_tpm = numpy.asarray(list(tpm.values()), dtype=numpy.float64)
     sumrz_tpm["TPM_0"] = len(np_tpm[np_tpm >= 0])
-    for i in range(-2,4):
+    for i in range(-2, 4):
         idx = pow(10, i)
         sumrz_tpm["TPM_" + str(idx)] = len(np_tpm[np_tpm >= idx])
 
-
-
     hk_tpm = get_hkgenes(filename_hk, target_genes, tpm)
 
-    return (sumrz_tpm, hk_tpm)
+    return sumrz_tpm, hk_tpm
+
 
 def write_output(sumrz_cnts, sumrz_tpm, hk_counts, hk_tpm, outfile, outjson):
     """
@@ -164,17 +147,15 @@ def write_output(sumrz_cnts, sumrz_tpm, hk_counts, hk_tpm, outfile, outjson):
         out.write('HOUSEKEEPING GENES\n')
         out.write('hgvs_gene_id\tensembl_id\tcounts\ttpm\n')
 
-        #sort by HUGO gene name, because life is hard
-        for key, value in sorted(hk_counts.iteritems(), key=lambda (k, v): (v, k)):
+        # sort by HUGO gene name, because life is hard
+        for key, value in sorted(hk_counts.items()):
             out.write('%s\t%s\t%s\t%s\n' % (value[0], key, str(value[1]), hk_tpm[key][1]))
 
         out.write('\n')
 
-
     with open(outjson, 'w') as jsonout:
-        json_dict = {"COUNTS": sumrz_cnts, "TPM": sumrz_tpm}
-        jsonout.write(json.dumps(json_dict, sort_keys=True, indent=4, separators=(',', ': ')))
-
+        json_dict = {**sumrz_cnts, **sumrz_tpm}
+        jsonout.write(json.dumps(json_dict))
 
 
 if __name__ == "__main__":
