5b9cc0972acfeab555cc0c06b2d1046b6d55fd02
letaw <jhl667@yahoo.com>
2021-03-23 13:19:51 -0700

    refactoring to prep for new snp profile functionality

--- cgd_client/snp_profile.py
+++ cgd_client/snp_profile.py
@@ -1,21 +1,120 @@
 from scipy.stats import binom_test
-import argparse
+import json
 import vcfpy
 
-VERSION = '0.2.1'
+VERSION = '0.3.0'
 
 
-def supply_args():
+class VcfRec:
     """
-    Populate args.
-    https://docs.python.org/2.7/library/argparse.html
+    Get details of a single variant, determine whether variant is "bad" or "good", based
+    on allele balance and read depth.
     """
-    parser = argparse.ArgumentParser(description='')
-    parser.add_argument('input1', help='First VCF input.')
-    parser.add_argument('input2', help='Second VCF input.')
-    parser.add_argument('--version', action='version', version='%(prog)s ' + VERSION)
-    args = parser.parse_args()
-    return args
+    def __init__(self, vrnt):
+        self.vrnt = vrnt
+        self.bad_dp = self._bad_dp()
+        self.geno = self._trans_geno()
+        self.bad_ab = self._bad_ab()
+        self.bad_fs = self._bad_fs()
+        self.final = self._set_final_geno()
+
+    def _set_final_geno(self):
+        """
+
+        :return:
+        """
+        if self.bad_dp or self.bad_ab or self.bad_fs:
+            return -1
+        else:
+            return self.geno
+
+    def _bad_fs(self, thresh=5):
+        """
+        Detemrine whether the variant sample DP is below "thresh".
+        :return:
+        """
+        if 'FS' in self.vrnt.INFO:
+            fs = self.vrnt.INFO['FS']
+        else:
+            return True
+
+        if fs <= thresh:
+            return False
+        return True
+
+    def _bad_dp(self, thresh=40):
+        """
+        Detemrine whether the variant sample DP is below "thresh".
+        :return:
+        """
+        dp = [call.data.get('DP') for call in self.vrnt.calls][0]
+        if not dp:
+            return True
+        if dp >= thresh:
+            return False
+        return True
+
+    def _bad_ab(self):
+        try:
+            ref = [call.data.get('AD') for call in self.vrnt.calls][0][0]
+            alt = [call.data.get('AD') for call in self.vrnt.calls][0][1]
+            ab = self._calc_ab(ref, alt, self.geno)
+        except:
+            ab = None
+        return self._assess_ab(ab, self.geno)
+
+    def _trans_geno(self):
+        """
+        Translate genotype from what is listed in the VCF.
+        :return:
+        """
+        geno = [call.data.get('GT') for call in self.vrnt.calls][0]
+        return self._get_geno(geno)
+
+    @staticmethod
+    def _get_geno(geno):
+        """
+        For genotypes, set values 0, 1, and 2.
+        :return:
+        """
+        if geno == '0/0':
+            return 0
+        elif geno == '0/1':
+            return 1
+        elif geno == '1/1':
+            return 2
+        else:
+            return -1
+
+    @staticmethod
+    def _calc_ab(ref, alt, geno):
+        """
+        Calculate the allele balance.
+        :return:
+        """
+        if geno == 0:
+            return float(alt / (ref + alt + 0.0))
+        elif geno == 1:
+            return abs(0.5 - (float(alt / (ref + alt + 0.0))))
+        elif geno == 2:
+            return float(ref / (ref + alt + 0.0))
+        else:
+            return None
+
+    @staticmethod
+    def _assess_ab(ab, geno, hom_thresh=0.01, het_thresh=0.1):
+        """
+        """
+        if ab is None or geno is None:
+            return True
+        elif ab > hom_thresh and geno == 0:
+            return True
+        elif ab > het_thresh and geno == 1:
+            return True
+        elif ab > hom_thresh and geno == 2:
+            return True
+        else:
+            return False
 
 
 class SnpProfile(object):
@@ -35,8 +134,8 @@ class SnpProfile(object):
     def __init__(self, filename):
 
         self.vcf_read = vcfpy.Reader.from_path(filename)
-        self.ab_dict = {}
         self.geno_items = self._vcf_parse()
+
 
     def _vcf_parse(self):
         """
@@ -45,67 +144,17 @@ class SnpProfile(object):
         """
         geno_items = []
         for record in self.vcf_read:
+            vrnt = VcfRec(record)
             samp_cnt = len([call for call in record.calls])
             chrom = str(record.CHROM)
             pos = int(record.POS)
             if samp_cnt == 1:
-                geno = self._get_geno([call.data.get('GT') for call in record.calls][0])
-                ref = [call.data.get('AD') for call in record.calls][0][0]
-                alt = [call.data.get('AD') for call in record.calls][0][1]
-                ab = self._calc_ab(ref, alt, geno)
-                self.ab_dict[(chrom, pos)] = ab
-                geno = self._assess_ab(ab, geno)
+                geno = vrnt.final
             else:
                 raise Exception("The input VCF should only have one sample column, this one has " +
                                 str(len(record.samples)))
             geno_items.append({"chromosome": chrom, "position": pos, "genotype": geno})
-
         return geno_items
-
-    @staticmethod
-    def _get_geno(geno):
-        """
-        For genotypes, set values 0, 1, and 2.
-        :return:
-        """
-        if geno == '0/0':
-            return 0
-        elif geno == '0/1':
-            return 1
-        elif geno == '1/1':
-            return 2
-        else:
-            return -1
-
-    @staticmethod
-    def _assess_ab(ab, geno, hom_thresh=0.01, het_thresh=0.1):
-        """
-        """
-        if ab is None or geno is None:
-            return -1
-        elif ab > hom_thresh and geno == 0:
-            return -1
-        elif ab > het_thresh and geno == 1:
-            return -1
-        elif ab > hom_thresh and geno == 2:
-            return -1
-        else:
-            return geno
-
-    @staticmethod
-    def _calc_ab(ref, alt, geno):
-        """
-        Calculate the allele balance.
-        :return:
-        """
-        if geno == 0:
-            return float(alt / (ref + alt + 0.0))
-        elif geno == 1:
-            return abs(0.5 - (float(alt / (ref + alt + 0.0))))
-        elif geno == 2:
-            return float(ref / (ref + alt + 0.0))
-        else:
-            return None
 
 
 class CompareProfiles(object):
@@ -121,6 +170,7 @@ class CompareProfiles(object):
         self._compare_them()
         self.for_cgd = self.dict_to_json(self.new_snps)
         self.pvalue = self._perform_binom()
+        # print(f"There were {str(self.mismatch)} mismatches and {str(self.total)} total compared loci.")
 
     @staticmethod
     def _create_dict(prof):
@@ -167,9 +217,13 @@ class CompareProfiles(object):
         """
         pvalue = binom_test(self.mismatch, self.total, prob, alternative='greater')
         if pvalue <= pfail:
-            print(f"Null hypothesis(p={str(pfail)}) of sample profiles being the same is rejected: {str(pvalue)}")
+            pass
+            # print(f"Null hypothesis(p={str(pfail)}) of sample profiles being the same is rejected: {str(pvalue)}")
         else:
-            print(f"Null hypothesis(p={str(pfail)}) of sample profiles being the same is confirmed: {str(pvalue)}")
+            # print(f"Null hypothesis(p={str(pfail)}) of sample profiles being the same is confirmed: {str(pvalue)}.")
+            print(f"There were {str(self.mismatch)} mismatches and {str(self.total)} total compared loci.")
+            if self.mm_loc:
+                print(self.mm_loc)
         return pvalue
 
     def _compare_them(self):
@@ -204,3 +258,17 @@ class CompareProfiles(object):
                 else:
                     # Situation 4
                     self.new_snps[pos] = geno
+
+
+class ProfileWrite:
+    def __init__(self, filename, genos):
+        self.filename = filename
+        self.genos = genos
+
+    def write_json(self):
+        """
+        Put the snp profile in a file.
+        :return:
+        """
+        with open(self.filename, 'w') as myfile:
+            json.dump(self.genos, myfile)
