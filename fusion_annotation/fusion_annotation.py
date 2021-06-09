98b613706a1224c34b5f5b840d77777b96637d5d
jhl667 <jhl667@yahoo.com>
2021-06-09 08:59:50 -0700

    a couple more gene filtering regexes

--- fusion_annotation/fusion_annotation.py
+++ fusion_annotation/fusion_annotation.py
@@ -9,7 +9,7 @@ from collections import OrderedDict
 from ensembldb import EnsemblDbImport
 from gtf import Gtf
 
-VERSION = '0.5.0'
+VERSION = '0.5.1'
 
 
 def supply_args():
@@ -373,12 +373,12 @@ def main():
                        "TRNI", "TRNK", "TRNL1", "TRNL2", "TRNM", "TRNN", "TRNP",
                        "TRNQ", "TRNR", "TRNS1", "TRNS2", "TRNT", "TRNV", "TRNW", "TRNY",
                        "IGH-@-ext", "IGH@", "IGH-@", "IGL-@", "IGL@", "ABC7-42389800N19.1", "IGH@-ext"]
-        regex_filt = [r'A[LC][0-9]{6}\.[0-9]{1}',
+        regex_filt = [r'A[PLC][0-9]{6}\.[0-9]{1,2}',
                       r'RP[0-9]{1,2}-[0-9]{1,4}[A-Z]{1}[0-9]{1,2}\.[0-9]{1,2}',
                       r'CT[CDAB]{1}-[0-9]{3,4}[A-Z]{1}[0-9]+.[0-9]+',
                       r'XX[ybacos]{0,3}-[0-9A-Z_]+\.[0-9]{1,2}',
                       r'GS1-[0-9]{2,3}[A-Z]{1}[0-9]{1,2}\.[0-9]{1,2}',
-                      r'hsa-mir-[0-9]+']
+                      r'hsa-mir-[0-9]+', r'U[0-9]{5}\.[0-9]{1,2}', r'MT-[A-Z0-9]{1,2,3,4}']
 
     else:
         hard_filter = None
