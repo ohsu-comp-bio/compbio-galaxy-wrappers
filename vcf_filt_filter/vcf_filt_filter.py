96f09b58936fdb04ace6887a935873d3cd00e2c1
jhl667 <jhl667@yahoo.com>
2022-06-01 11:03:58 -0700

    added filt_rm parameter

--- vcf_filt_filter/vcf_filt_filter.py
+++ vcf_filt_filter/vcf_filt_filter.py
@@ -6,7 +6,7 @@ Perform basic filtering on a VCF based on annotations in the FILTER column.
 import argparse
 import vcfpy
 
-VERSION = '0.0.2'
+VERSION = '0.0.3'
 
 
 def supply_args():
@@ -19,6 +19,7 @@ def supply_args():
                                                      'in the FILTER column.')
     parser.add_argument('--sing_vaf', type=float, help='AF to use when determining whether to drop a sing_rm record.')
     parser.add_argument('--only_sing', help='Only include this FILTER label if it is the only variant caller label.')
+    parser.add_argument('--filt_rm', help='Remove any record with this FILTER label.')
     parser.add_argument('--version', action='version', version='%(prog)s ' + VERSION)
     args = parser.parse_args()
     return args
@@ -53,6 +54,19 @@ def check_only_sing(only_sing, filt):
     return filt
 
 
+def check_filt_rm(filt, filt_check):
+    """
+
+    :param filt:
+    :param filt_check:
+    :return:
+    """
+    for f in filt_check.split(' '):
+        if f in filt:
+            return True
+    return False
+
+
 def main():
     args = supply_args()
     reader = vcfpy.Reader.from_path(args.input_vcf)
@@ -61,6 +75,7 @@ def main():
     writer_bad = vcfpy.Writer.from_path(args.output_vcf_filt, header=header)
     for vrnt in reader:
         filt = vrnt.FILTER
+        filt_add_back = []
         vaf = float(vrnt.calls[0].data['AF'])
         low_vaf = False
         if args.sing_vaf:
@@ -68,11 +83,19 @@ def main():
         for f in filt:
             if f not in args.callers.split(' '):
                 filt.remove(f)
+                filt_add_back.append(f)
         res = check_sing_filt(args.sing_rm.split(' '), filt, low_vaf)
         if args.only_sing:
             vrnt.FILTER = check_only_sing(args.only_sing, filt)
         if not res:
-            writer.write_record(vrnt)
+            vrnt.FILTER.extend(filt_add_back)
+            crm = False
+            if args.filt_rm:
+                crm = check_filt_rm(vrnt.FILTER, args.filt_rm)
+            if not crm:
+                writer.write_record(vrnt)
+            else:
+                writer_bad.write_record(vrnt)
         else:
             writer_bad.write_record(vrnt)
     writer.close()
