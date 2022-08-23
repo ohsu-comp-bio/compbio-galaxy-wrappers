9c855231d8a4ac34aba6123ea2519a7c0cecac55
ok-gitr <kosisoonwuzu@gmail.com>
2022-08-23 15:27:27 -0700

    update filter_strand to take a merged VCF

--- filter_strand/filter_strand.py
+++ filter_strand/filter_strand.py
@@ -13,7 +13,7 @@ import argparse
 import vcfpy
 import numpy as np
 
-VERSION = '1.0.0'
+VERSION = '1.2.0'
 
 
 def supply_args():
@@ -22,11 +22,39 @@ def supply_args():
     https://docs.python.org/2.7/library/argparse.html
     """
     parser = argparse.ArgumentParser(description='')
-    parser.add_argument('infile', help='Input VCF.')
-    parser.add_argument('outfile', help='Output VCF.')
+    parser.add_argument('--infile', help='Input VCF.')
+    parser.add_argument('--vcftype', help='Type of VCF.')
+    parser.add_argument('--callers', help='Callers in a merged VCF.')
+    parser.add_argument('--outfile', help='Output VCF.')
     parser.add_argument('--version', action='version', version='%(prog)s ' + VERSION)
     args = parser.parse_args()
     return args
+
+
+def m2_strand_count(record):
+    srf, srr, saf, sar = record.calls[0].data['SB']
+    return [srf, srr, saf, sar]
+
+
+def fb_strand_count(record):
+    srf, srr, saf, sar, = record.INFO['SRF'], record.INFO['SRR'], record.INFO['SAF'][0], record.INFO['SAR'][0]
+    return [srf, srr, saf, sar]
+
+
+def get_strand_count(record, vcftype, callers):
+    if vcftype == 'merged':
+        callers = callers.split(' ')
+        for flt in record.FILTER:
+            if flt in callers:
+                vcftype = flt[:2]
+                break
+    if vcftype == 'm2':
+        return m2_strand_count(record)
+    elif vcftype == 'fb':
+        return fb_strand_count(record)
+    else:
+        pass
+        # raise Exception('Unrecognized VCF type at {}:{}'.format(record.CHROM, record.POS))
 
 
 class VcfReader:
@@ -52,33 +80,36 @@ class StrandOps:
         Check SAF, SAR, SRR, SRF fields to find out if we should call this StrandBias.
         :return:
         """
-        saf = self.info['SAF'][0]
-        sar = self.info['SAR'][0]
-        srf = self.info['SRF']
-        srr = self.info['SRR']
-        alt_dp = self._alt_dp(saf, sar)
-        hoeff = self._hoeffding_t(alt_dp, conf=0.99)
-        ref_vaf = self._strand_freq(srf, srr)
-        alt_vaf = self._strand_freq(saf, sar)
-        if ref_vaf:
-            lower = max(ref_vaf - hoeff, 0)
-            upper = min(ref_vaf + hoeff, 1)
+        if self.info:
+            srf = self.info[0]
+            srr = self.info[1]
+            saf = self.info[2]
+            sar = self.info[3]
+            alt_dp = self._alt_dp(saf, sar)
+            hoeff = self._hoeffding_t(alt_dp, conf=0.99)
+            ref_vaf = self._strand_freq(srf, srr)
+            alt_vaf = self._strand_freq(saf, sar)
+            if ref_vaf:
+                lower = max(ref_vaf - hoeff, 0)
+                upper = min(ref_vaf + hoeff, 1)
+            else:
+                lower = 0
+                upper = 1
+
+            # if saf != 0 and sar != 0:
+            #     print("SAF: {0}".format(saf))
+            #     print("SAR: {0}".format(sar))
+            #     print("SRF: {0}".format(srf))
+            #     print("SRR: {0}".format(srr))
+            #     print("Lower: {0}".format(lower))
+            #     print("Upper: {0}".format(upper))
+            #     print("Alt VAF: {0}".format(alt_vaf))
+
+            if lower <= alt_vaf <= upper:
+                return True
+            return False
         else:
-            lower = 0
-            upper = 1
-
-        # if saf != 0 and sar != 0:
-        #     print("SAF: {0}".format(saf))
-        #     print("SAR: {0}".format(sar))
-        #     print("SRF: {0}".format(srf))
-        #     print("SRR: {0}".format(srr))
-        #     print("Lower: {0}".format(lower))
-        #     print("Upper: {0}".format(upper))
-        #     print("Alt VAF: {0}".format(alt_vaf))
-
-        if lower <= alt_vaf <= upper:
-            return True
-        return False
+            return False
 
     @staticmethod
     def _hoeffding_t(n, conf=0.95):
@@ -88,8 +119,13 @@ class StrandOps:
         :param n:
         :return:
         """
+        # Strand count can sometimes be 0 in mutect2
+        # https://gatk.broadinstitute.org/hc/en-us/articles/360035532252-Allele-Depth-AD-is-lower-than-expected
 
-        return np.sqrt((-1/(2*n))*(np.log((1-conf)/2)))
+        try:
+            return np.sqrt((-1/(2*n))*(np.log((1-conf)/2)))
+        except ZeroDivisionError:
+            return 0
 
     @staticmethod
     def _set_epsilon(fwd, rev, fac=2):
@@ -148,11 +184,14 @@ def main():
     writer = vcfpy.Writer.from_path(args.outfile, infile.header)
 
     for entry in infile.vrnts:
-        strand = StrandOps(entry.INFO)
-        strand_res = strand.assess_strand()
-        if not strand_res:
-            filt = FilterAdd(entry.FILTER)
-            filt.add_filt(text=filter_annot)
+        info = get_strand_count(entry, args.vcftype, args.callers)
+        if info:
+            strand = StrandOps(info)
+            strand_res = strand.assess_strand()
+            if not strand_res:
+                #print(entry.CHROM, entry.POS, entry.FILTER)
+                filt = FilterAdd(entry.FILTER)
+                filt.add_filt(text=filter_annot)
         writer.write_record(entry)
 
     writer.close()
