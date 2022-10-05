caf32857bba8cda569e6239306cd1abfc72816d9
jhl667 <jhl667@yahoo.com>
2022-10-05 12:36:17 -0700

    fixed spelling of green variable, removed example section

--- idat_reader/idat_reader.R
+++ idat_reader/idat_reader.R
@@ -7,14 +7,9 @@ library(tibble)
 ### START ARGS
 args <- commandArgs(trailingOnly=TRUE)
 red <- args[1]
-reen <- args[2]
+green <- args[2]
 manifest <- args[3]
 output_filename <- args[4]
-
-# Example
-red <-'./idat/Galaxy2-[206112280126_R08C01_Red.idat].idat'
-green<-'./idat/Galaxy1-[206112280126_R08C01_Grn.idat].idat'
-manifest<-'infinium-methylationepic-v-1-0-b5-manifest-file.csv'
 
 # Read in .idat files
 idat_red <- readIDAT(red)
@@ -26,7 +21,7 @@ colnames(red.counts) <- c('IlmnID','red.mean','red.sd','red.NBead')
 green.counts <- tibble::rownames_to_column(data.frame(idat_green$Quants), "IlmnID")
 colnames(green.counts) <- c('IlmnID','green.mean','green.sd','green.NBead')
 total.counts <- merge(red.counts,green.counts,by='IlmnID')
-total.counts$IlmnID = paste0('cg', total.counts$IlmnID)
+total.counts$IlmnID <- paste0('cg', total.counts$IlmnID)
 
 # Load in Illumina Methylation manifest + write to text
 mani <- read.csv(manifest,skip=7)
