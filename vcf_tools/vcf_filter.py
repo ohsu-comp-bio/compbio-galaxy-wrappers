7101f40cddca4adf6ea7a80a3796c3e6f13c589e
ok-gitr <kosisoonwuzu@gmail.com>
2022-11-04 01:00:08 -0700

    update vcf_tools version numbers

---
+++ vcf_tools/vcf_filter.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python
+
+"""
+Filter a merged VCF based on FILTER column.
+
+Usage:
+    vcf_filter.py 'input.vcf' 'output.vcf' 'output2_vcf'
+        --callers "sc fb m2 fc mrdf"
+        --exclude "PON_FILT" --include "mrdf fc clinvar.CLNSIG=Pathogenic clinvar.CLNSIG=Likely_pathogenic clinvar.CLNSIG=Pathogenic/Likely_pathogenic CLNSIGCONF=Pathogenic CLNSIGCONF=Likely_pathogenic" --snp_threshold 1.0 --indel_threshold 1.0
+        --exclude "BelowBKGD" --include "mrdf fc clinvar.CLNSIG=Pathogenic clinvar.CLNSIG=Likely_pathogenic clinvar.CLNSIG=Pathogenic/Likely_pathogenic CLNSIGCONF=Pathogenic CLNSIGCONF=Likely_pathogenic" --snp_threshold 1.0 --indel_threshold 1.0
+        --exclude "StrandBias" --include "mrdf fc clinvar.CLNSIG=Pathogenic clinvar.CLNSIG=Likely_pathogenic clinvar.CLNSIG=Pathogenic/Likely_pathogenic CLNSIGCONF=Pathogenic CLNSIGCONF=Likely_pathogenic" --snp_threshold 1.0 --indel_threshold 1.0
+        --exclude "fb" --include "mrdf fc clinvar.CLNSIG=Pathogenic clinvar.CLNSIG=Likely_pathogenic clinvar.CLNSIG=Pathogenic/Likely_pathogenic CLNSIGCONF=Pathogenic CLNSIGCONF=Likely_pathogenic" --snp_threshold 0.1 --indel_threshold 0.02
+        --exclude "m2"  --include "mrdf fc clinvar.CLNSIG=Pathogenic clinvar.CLNSIG=Likely_pathogenic clinvar.CLNSIG=Pathogenic/Likely_pathogenic clinvar.CLNSIGCONF=Pathogenic clinvar.CLNSIGCONF=Likely_pathogenic" --snp_threshold 0.1 --indel_threshold 0.1
+
+Given a merged VCF, this will filter out variants that contain the exclude argument in the FILTER column.
+"""
+
+import argparse
+import vcfpy
+
+VERSION = '2.0.0'
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('input_vcf', help='Input VCF')
+    parser.add_argument('output_vcf', help='Output VCF')
+    parser.add_argument('output2_vcf', help='Output VCF of filtered out variants')
+    parser.add_argument('--callers', help='FILTER annotations of variant callers used in the VCF')
+    parser.add_argument('--exclude',  action='append', help='Filters to use to exclude VCF records')
+    parser.add_argument('--include', action='append', help='Filters to use to include VCF records')
+    parser.add_argument('--snp_threshold', action='append', type=float, help='For SNPs, only perform filtering if VAF is below this threshold')
+    parser.add_argument('--indel_threshold', action='append', type=float, help='For INDELs, only perform filtering if VAF is below this threshold')
+    parser.add_argument('-v', '--version', action='version', version="%(prog)s " + VERSION)
+
+    args = parser.parse_args()
+
+    if len(args.exclude) != len(args.snp_threshold) != len(args.snp_threshold):
+        raise Exception('Must specify a snp_threshold and indel_threshold arguments for each exclude argument')
+    return args
+
+
+class RecordFilters:
+    def __init__(self, record_filters, vcf_callers):
+        self.filters = record_filters
+        self.vcf_callers = vcf_callers
+        self.callers = [filt for filt in record_filters if filt in vcf_callers]
+        self.flags = [filt for filt in record_filters if filt not in vcf_callers]
+        self.info = self._get_info()
+
+    def _get_info(self):
+        info_dict = {}
+        for filt in self.filters:
+            if '=' in filt:
+                k, v = filt.split('=')
+                if k not in info_dict:
+                    info_dict[k] = [v]
+                else:
+                    info_dict[k].append(v)
+        return info_dict
+
+    def caller_prioritize(self):
+        return self.callers + self.flags
+
+
+def check_info(record, info):
+    for k in info:
+        if k in record.INFO:
+            for item in info[k]:
+                # if given filter string is contained in record.INFO item  # TODO
+                if any(item in item2 for item2 in record.INFO[k]):
+                    return True
+
+
+class RecordKeeper:
+    def __init__(self, record, callers):
+        self.record = record
+        self.callers = callers
+        self.rec_filters = RecordFilters(record.FILTER, callers)
+
+    def exclude_record(self, exclude, threshold):
+        exclude = RecordFilters(exclude, self.callers)
+        if self.record.calls[0].data['AF'] < float(threshold):
+            # if only given a caller check that it is the only caller present
+            if exclude.callers and not (exclude.flags or exclude.info):
+                if exclude.callers == self.rec_filters.callers:
+                    return True
+            # if given only flag(s) (or info field(s))
+            elif not exclude.callers and (exclude.flags or exclude.info):
+                if any(filt in self.rec_filters.filters for filt in exclude.flags) or check_info(self.record, exclude.info):
+                    return True
+            # if given callers and flags or info fields
+            elif exclude.callers and (exclude.flags or exclude.info):
+                if exclude.callers == self.rec_filters.callers and (any(filt in self.rec_filters.filters for filt in exclude.flags) or check_info(self.record, exclude.info)):
+                    return True
+            else:
+                raise Exception('Filters must be callers, flags or info (given as ID=value)')
+        return False
+
+    def include_record(self, include):
+        include = RecordFilters(include, self.callers)
+        if any(filt in self.rec_filters.filters for filt in include.filters) or \
+                any(filt in self.rec_filters.filters for filt in include.filters) or \
+                check_info(self.record, include.info):
+            return True
+        return False
+
+    def keep_record(self, exclude, include, threshold):
+        if exclude and not include:
+            return not self.exclude_record(exclude, threshold)
+        elif include and not exclude:
+            return self.include_record(include)
+        elif exclude and include:
+            if self.include_record(include):
+                return True
+            else:
+                return not self.exclude_record(exclude, threshold)
+        else:
+            raise Exception('Filters must be callers, flags or info (given as ID=value)')
+
+
+def main():
+    args = get_args()
+
+    reader = vcfpy.Reader.from_path(args.input_vcf)
+    header = reader.header
+    writer = vcfpy.Writer.from_path(args.output_vcf, header=header)
+    writer2 = vcfpy.Writer.from_path(args.output2_vcf, header=header)
+
+    callers = args.callers.split(' ')
+
+    for record in reader:
+        keep = True
+        if args.exclude:
+            for i in range(len(args.exclude)):
+                exclude = args.exclude[i].split(' ')
+                try:
+                    include = args.include[i].split(' ')
+                except IndexError:
+                    include = []
+
+                if keep:  # if a record has not been excluded by a preceding exclude argument
+                    recordkeeper = RecordKeeper(record, callers)
+                    if record.ALT[0].type == 'SNV':
+                        keep = recordkeeper.keep_record(exclude, include, float(args.snp_threshold[i]))
+                    else:
+                        keep = recordkeeper.keep_record(exclude, include, float(args.indel_threshold[i]))
+                else:
+                    break
+
+        record.FILTER = RecordFilters(record.FILTER, callers).caller_prioritize()
+        if keep:
+            writer.write_record(record)
+        else:
+            writer2.write_record(record)
+
+
+if __name__ == "__main__":
+    main()
--- vcf_tools/vcf_merge.py
+++ vcf_tools/vcf_merge.py
@@ -1,34 +1,148 @@
+#!/usr/bin/env python
+
 """
 Merge VCFs produced by various variant callers.
 
-Example usage: vcf_merge.py  --input_vcfs file1 file2 --caller_labels m2 fb --output_vcf output.vcf
+Usage:
+    vcf_merge.py  --input_vcfs input1.vcf input2.vcf --caller_labels m2 fb --output_vcf output.vcf
 
-Details: Given multiple vcfs produced by various variant callers, this will merge all vcfs into one.
-For variants called by multiple callers, it will output a single record and the info column and sample column
+Details:
+Given multiple VCFs produced by various variant callers, this will merge all vcfs into one.
+For variants called by multiple callers, it will output a single record and the filter, info column and sample column
 will contain those of all callers that made the call.
 """
 
 import argparse
-import vcf_tools
+import vcfpy
+from operator import attrgetter
+from natsort import natsorted
 
-VERSION = vcf_tools.VERSION+'.0'
+VERSION = '2.0.0'
 
 
-def main():
-    parser = argparse.ArgumentParser(
-        description="vcf_merge.py --input_vcfs 'file1.vcf' 'file2.vcf' 'fileN.vcf' "
-                    "--caller_labels label1 label2 labelN "
-                    "--output_vcf 'output.vcf'")
+class VcfMerger:
+    """
+    Merge headers and variants in VCFs produced by different variant callers
+    """
+
+    def __init__(self, input_vcfs, callers, vcf_format="VCFv4.2"):
+        self.readers = [vcfpy.Reader.from_path(vcf) for vcf in input_vcfs]
+        self.callers = callers
+        self.vcf_format = vcf_format
+        self.samples = list(set([name for reader in self.readers for name in reader.header.samples.names]))
+        self.new_filter_ids = list(
+            set(['{}_{}'.format(caller, flt) for reader, caller in zip(self.readers, self.callers) for flt in
+                 reader.header.filter_ids()]))
+        self.new_info_ids = list(
+            set(['{}_{}'.format(caller, info) for reader, caller in zip(self.readers, self.callers) for info in
+                 reader.header.info_ids()]))
+        self.new_format_ids = list(
+            set(['{}_{}'.format(caller, fmt) for reader, caller in zip(self.readers, self.callers) for fmt in
+                 reader.header.format_ids()]))
+
+    @staticmethod
+    def add_header(vfield, caller, field, ids):
+        if vfield in ['INFO', 'FORMAT']:
+            field = '{}_{}'.format(caller, field)
+        if field not in ['.', 'PASS']:
+            if field not in ids:
+                return True
+    
+    @staticmethod
+    def update_header(caller, field):
+        field_odict = vcfpy.OrderedDict()
+        for k, v in field.mapping.items():
+            if k == 'ID':
+                v = '{}_{}'.format(caller, v)
+            if k == 'Description':
+                v = '{} {}'.format(caller, v)
+            field_odict[k] = v
+        # k, v = field_odict['ID'], '<{}>'.format(','.join(str(k)+'='+str(v) for k, v in field_odict.items()))
+        return field_odict
+
+    def merge_headers(self):
+        new_header = vcfpy.Header(samples=[reader.header.samples for reader in self.readers][0])
+        new_header.add_line(vcfpy.HeaderLine('fileformat', self.vcf_format))
+
+        new_contig_headers = {}
+        for reader, caller in zip(self.readers, self.callers):
+            contig_headers = {contig.id: contig for contig in reader.header.get_lines('contig')}
+            for contig in contig_headers:
+                if self.add_header('contig', caller, contig, new_contig_headers):
+                    new_contig_headers[contig_headers[contig].id] = contig_headers[contig]
+                    new_header.add_contig_line(contig_headers[contig].mapping)
+            new_header.add_filter_line(vcfpy.OrderedDict([('ID', caller), ('Description', 'Variant caller label.')]))
+            filter_headers = {flt.id: flt for flt in reader.header.get_lines('FILTER')}
+            for flt in filter_headers:
+                if self.add_header('FILTER', caller, flt, new_header.filter_ids()):
+                    new_header.add_filter_line(filter_headers[flt].mapping)
+            info_headers = {info.id: info for info in reader.header.get_lines('INFO')}
+            for info in info_headers:
+                if self.add_header('INFO', caller, info, new_header.info_ids()):
+                    new_header.add_info_line(self.update_header(caller, info_headers[info]))
+            format_headers = {fmt.id: fmt for fmt in reader.header.get_lines('FORMAT')}
+            for fmt in format_headers:
+                if self.add_header('FORMAT', caller, fmt, new_header.format_ids()):
+                    new_header.add_format_line(self.update_header(caller, format_headers[fmt]))
+        return new_header
+
+    @staticmethod
+    def update_fields(caller, field_dict, rec_field_dict):
+        for field in field_dict.keys():
+            c, k = field.split('_', 1)
+            if c == caller:
+                field_dict[field] = rec_field_dict.get(k, None)
+        return field_dict
+
+    def update_record(self, caller, new_record, record, samples):
+        new_record.add_filter(caller)
+        new_record.INFO = self.update_fields(caller, new_record.INFO, record.INFO)
+        for sample in samples:
+            for fmt in new_record.FORMAT.keys():
+                new_record.add_format(fmt)
+            new_record.call_for_sample[sample].data = self.update_fields(caller, new_record.call_for_sample[sample].data, record.call_for_sample[sample].data)
+        return new_record
+
+    def merge_records(self):
+        records = {}
+        for reader, caller in zip(self.readers, self.callers):
+            for record in reader:
+                new_record = vcfpy.Record(CHROM=record.CHROM,
+                                          POS=record.POS,
+                                          ID=record.ID,
+                                          REF=record.REF,
+                                          ALT=record.ALT,
+                                          QUAL=(None if record.QUAL == float('inf') else record.QUAL),  # happens with fc
+                                          FILTER=([flt for flt in record.FILTER if flt not in ['.', 'PASS']]),
+                                          INFO=dict.fromkeys(self.new_info_ids, None),
+                                          FORMAT=dict.fromkeys(self.new_format_ids, None),
+                                          calls=[vcfpy.Call(sample, dict.fromkeys(self.new_format_ids, None)) for sample in reader.header.samples.names])
+                var_id = '{}:{}{}>{}'.format(record.CHROM, record.POS, record.REF, record.ALT)
+                if var_id not in records:
+                    records[var_id] = self.update_record(caller, new_record, record, reader.header.samples.names)
+                else:
+                    records[var_id] = self.update_record(caller, records[var_id], record, reader.header.samples.names)
+        return list(records.values())
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description="")
     parser.add_argument('--input_vcfs', nargs="+", help="Input VCFs")
     parser.add_argument('--caller_labels', nargs="+", help="Labels for each input vcf")
     parser.add_argument('--output_vcf', help="Output VCF.")
     parser.add_argument('-v', '--version', action='version', version="%(prog)s " + VERSION)
     args = parser.parse_args()
+    return args
 
-    merge = vcf_tools.VcfMerger(args.input_vcfs, args.caller_labels)
-    header = merge.merge_headers()
-    records = merge.merge_records()
-    vcf_tools.VarWriter(records).as_vcf(args.output_vcf, header)
+
+def main():
+    args = get_args()
+
+    merge = VcfMerger(args.input_vcfs, args.caller_labels)
+
+    writer = vcfpy.Writer.from_path(args.output_vcf, header=merge.merge_headers())
+    for record in natsorted(merge.merge_records(), key=attrgetter('CHROM', 'POS')):
+        writer.write_record(record)
 
 
 if __name__ == "__main__":
