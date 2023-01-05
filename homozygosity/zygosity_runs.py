6ea5e90555af1c3596f18103685426521f06e50f
jhl667 <jhl667@yahoo.com>
2023-01-05 08:58:45 -0800

    updates eleanor never pushed

--- homozygosity/zygosity_runs.py
+++ homozygosity/zygosity_runs.py
@@ -2,13 +2,18 @@
 
 import argparse
 
-VERSION = '1.1.0'
+VERSION = '1.3.0'
 
 
 def supply_args():
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('input_file', help='input file')
-    parser.add_argument('--cut_off', default=50)
+    #parser.add_argument('--cut_off', default=50)
+    parser.add_argument('--flag_bp', default=9000000)
+    parser.add_argument('--flag_len', default=40)
+    parser.add_argument('--list_bp', default=3000000)
+    parser.add_argument('--list_len', default=25)
+    parser.add_argument('--list_num', default=15)
     args = parser.parse_args()
     return args
 
@@ -22,12 +27,13 @@ def is_hom(line):
     return False
 
 
-def get_runs(input_file, cut_off):
+def get_runs(input_file, flag_bp, flag_len, list_bp, list_len):
     with open(input_file, 'r') as geno_file:
         chrom = "CHROM"
         start = 0
         count = 0
         runs = []
+        flags = []
         sum_homs = 0.0
         sum_hets = 0.0
         homs = 0.0
@@ -53,25 +59,30 @@ def get_runs(input_file, cut_off):
             else:
                 hets = hets + 1
                 sum_hets = sum_hets + 1
-                if count >= cut_off:
+                if count >= list_len and not chrom == "X":
                     # and not chrom == "X"
                     end = line.split("\t")[1]
-                    runs.append((chrom, count, start, end))
+                    bp = int(end) - int(start)
+                    #print(bp)
+                    if bp >= list_bp:
+                        runs.append((chrom, count, start, end, bp))
+                    if count >= flag_len and bp >= flag_bp:
+                        flags.append((chrom, count, start, end, bp))
                 count = 0
                 start = line.split("\t")[1]
             
-    return [runs, chrom_ratios, float(sum_hets), float(sum_homs)]
+    return [runs, chrom_ratios, float(sum_hets), float(sum_homs), flags]
 
     
 def main():
     args = supply_args()
     with open(args.input_file) as f:
-      first_line = f.readline()
-      sample_gt = first_line.split("\t")[2]
-      sample_name = sample_gt.split(".")[0]
-      print(sample_name)
+        first_line = f.readline()
+        sample_gt = first_line.split("\t")[2]
+        sample_name = sample_gt.split(".")[0]
+        print(sample_name)
     
-    stuff = get_runs(args.input_file, int(args.cut_off))
+    stuff = get_runs(args.input_file, int(args.flag_bp), int(args.flag_len), int(args.list_bp), int(args.list_len))
     if stuff[3] == 0:
         stuff[3] = 1
     ratio = stuff[2]/stuff[3]
@@ -79,9 +90,23 @@ def main():
     for chrom in stuff[1]:
         print("Chr" + str(chrom[0]) + " Ratio: " + str(chrom[1]))
     stuff[0].sort(key=lambda tup: tup[1])
-    print("Homozygosity Runs Over " + str(args.cut_off) + ": ")
+    print("Homozygosity Runs Over " + str(args.list_len) + ": ")
     for run in stuff[0]:
-        print(str(run[1]) + " at chr" + str(run[0]) + ":" + str(run[2]) + "-" + str(run[3]))
+        print(str(run[1]) + " at chr" + str(run[0]) + ":" + str(run[2]) + "-" + str(run[3]) + " of length: " + str(run[4]))
+    print("Flagged Runs:")
+    for run in stuff[4]:
+        print(str(run[1]) + " at chr" + str(run[0]) + ":" + str(run[2]) + "-" + str(run[3]) + " of length: " + str(run[4]))
+        
+    flag_file = open("flag_file.json", "w")
+    
+    if len(stuff[0]) >= int(args.list_num):
+        flag_file.write('{"homozygosity_flag": 1}')
+    elif len(stuff[4]) > 0:
+        flag_file.write('{"homozygosity_flag": 1}')
+    else:
+        flag_file.write('{"homozygosity_flag": 0}')
+    
+    flag_file.close()
     
     
 if __name__ == "__main__":
