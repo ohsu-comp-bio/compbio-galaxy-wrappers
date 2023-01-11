ccee3dca7a7fb44e92f9fcb6135ff715910a39d7
Jay <22547630+jPleyte@users.noreply.github.com>
2023-01-11 15:53:02 -0800

    CGD-1984 - Change everything that uses the word "heme".
    This script originally relied on an sql query that we call the "heme
    variant export". The latest query is no longer similar to that heme
    export.

--- cgd_tx_eff/src/edu/ohsu/compbio/txeff/util/compare_cgd_tfx.py
+++ cgd_tx_eff/src/edu/ohsu/compbio/txeff/util/compare_cgd_tfx.py
@@ -1,7 +1,7 @@
 '''
 Created on Jul 1, 2022
 
-Takes as input a VCF that has been updated with transcript effects, and a heme variant export CSV, and 
+Takes as input a VCF that has been updated with transcript effects, and a variant export CSV, and 
 joins the two so we can compare the nomenclature used in transcript effects with what is currently in CGD. 
 
 @author: pleyte
@@ -53,7 +53,7 @@ def find_in_vcf(chromosome: str, position_start: int, position_end: int, ref: st
 
     return None
 
-def process(vcf_filename: str, csv_heme_filename: str, csv_out_filename: str):
+def process(vcf_filename: str, csv_export_filename: str, csv_out_filename: str):
     out_fields = ['genomic_variant_id', 'chromosome', 'position', 'reference', 'alt',
                   'cgd_variant_effect', 'cgd_variant_type', 'cgd_hgvs_amino_acid_position', 'cgd_hgvs_base_position', 
                   'cgd_exon', 'cgd_hgnc_gene', 'cgd_primary_gene', 'cgd_hgvs_c_dot', 'cgd_hgvs_p_dot_one', 'cgd_hgvs_p_dot_three', 
@@ -65,52 +65,52 @@ def process(vcf_filename: str, csv_heme_filename: str, csv_out_filename: str):
     logger.info(f"Reading {vcf_filename}")
     vcf_reader = vcfpy.Reader.from_path(vcf_filename)
     
-    logger.info(f"Reading {csv_heme_filename}")
+    logger.info(f"Reading {csv_export_filename}")
     
-    with open(csv_heme_filename) as csv_heme_file, open(csv_out_filename, "w") as csv_out_file:
-        # Open the input heme variant export csv 
-        heme_reader = csv.DictReader(csv_heme_file)
+    with open(csv_export_filename) as csv_export_file, open(csv_out_filename, "w") as csv_out_file:
+        # Open the variant export csv 
+        export_reader = csv.DictReader(csv_export_file)
 
         # Open the output csv
         csv_out_writer = csv.writer(csv_out_file)
         csv_out_writer.writerow(out_fields)
         
-        # Iterate over each entry in the heme export 
-        for heme_row in heme_reader:
-            logger.debug(f"Looking for {heme_row['chromosome']}-{heme_row['position_start']}-{heme_row['reference_base']}-{heme_row['variant_base']}")
+        # Iterate over each entry in the export file 
+        for export_row in export_reader:
+            logger.debug(f"Looking for {export_row['chromosome']}-{export_row['position_start']}-{export_row['reference_base']}-{export_row['variant_base']}")
             
-            vc = find_in_vcf(heme_row['chromosome'], int(heme_row['position_start']), int(heme_row['position_end']), heme_row['reference_base'], heme_row['variant_base'], vcf_reader)
+            vc = find_in_vcf(export_row['chromosome'], int(export_row['position_start']), int(export_row['position_end']), export_row['reference_base'], export_row['variant_base'], vcf_reader)
             if (vc == None):
-                logger.info(f"Unable to find genotype {heme_row['chromosome']}-{heme_row['position_start']}-{heme_row['reference_base']}-{heme_row['variant_base']} in vcf")
+                logger.info(f"Unable to find genotype {export_row['chromosome']}-{export_row['position_start']}-{export_row['reference_base']}-{export_row['variant_base']} in vcf")
                 continue
             
             if vc.INFO.get(TranscriptEffect.TFX_VARIANT_EFFECT.value) == None:
-                logger.info(f"Variant does not have variant effects: {heme_row['chromosome']}-{heme_row['position_start']}-{heme_row['reference_base']}-{heme_row['variant_base']}")
+                logger.info(f"Variant does not have variant effects: {export_row['chromosome']}-{export_row['position_start']}-{export_row['reference_base']}-{export_row['variant_base']}")
                 continue
             
             # Write row(s) related to this genotype to the csv 
-            write(csv_out_writer, heme_row, vc)
+            write(csv_out_writer, export_row, vc)
 
     logger.info(f"Wrote {csv_out_filename}")
 
-def write(csv_writer, heme_row, vc: vcfpy.record.Record):
+def write(csv_writer, export_row, vc: vcfpy.record.Record):
     '''
     '''    
-    csv_writer.writerow([heme_row['genomic_variant_id'],
-                         heme_row['chromosome'], heme_row['position_start'], heme_row['reference_base'], heme_row['variant_base'],
-                         heme_row['protein_variant_type'],                          # 'cgd_variant_effect' - annovar variant_effect is cgd's protein variant type
-                         heme_row['genomic_variant_type'],                          # 'cgd_variant_type'
-                         heme_row['amino_acid_position'],                           # 'cgd_hgvs_amino_acid_position' 
-                         heme_row['base_pair_position'],                            # 'cgd_hgvs_base_position', 
-                         heme_row['exon'],                                          # 'cgd_exon'
-                         heme_row['genes'],                                         # 'cgd_hgnc_gene'
-                         heme_row['primary_gene'],                                   # 'cgd_primary_gene'
-                         heme_row['genotype_cdna'],                                 # 'cgd_hgvs_c_dot'
-                         heme_row['genotype_amino_acid_onel'],                      # 'cgd_hgvs_p_dot_one' 
-                         heme_row['genotype_amino_acid_threel'],                    # 'cgd_hgvs_p_dot_three' 
-                         heme_row['splice_site_relationship'],                      # 'cgd_splicing'
-                         heme_row['cdna_transcript'],                               # 'cgd_refseq_transcript'
-                         heme_row['protein_transcript'],                           # 'cgd_protein_transcript'
+    csv_writer.writerow([export_row['genomic_variant_id'],
+                         export_row['chromosome'], export_row['position_start'], export_row['reference_base'], export_row['variant_base'],
+                         export_row['protein_variant_type'],                          # 'cgd_variant_effect' - annovar variant_effect is cgd's protein variant type
+                         export_row['genomic_variant_type'],                          # 'cgd_variant_type'
+                         export_row['amino_acid_position'],                           # 'cgd_hgvs_amino_acid_position' 
+                         export_row['base_pair_position'],                            # 'cgd_hgvs_base_position', 
+                         export_row['exon'],                                          # 'cgd_exon'
+                         export_row['genes'],                                         # 'cgd_hgnc_gene'
+                         export_row['primary_gene'],                                   # 'cgd_primary_gene'
+                         export_row['genotype_cdna'],                                 # 'cgd_hgvs_c_dot'
+                         export_row['genotype_amino_acid_onel'],                      # 'cgd_hgvs_p_dot_one' 
+                         export_row['genotype_amino_acid_threel'],                    # 'cgd_hgvs_p_dot_three' 
+                         export_row['splice_site_relationship'],                      # 'cgd_splicing'
+                         export_row['cdna_transcript'],                               # 'cgd_refseq_transcript'
+                         export_row['protein_transcript'],                           # 'cgd_protein_transcript'
                         ' / '.join([x for x in vc.INFO[TranscriptEffect.TFX_VARIANT_EFFECT.value]]),         # 'tfx_variant_effect'
                         ' / '.join([x for x in vc.INFO[TranscriptEffect.TFX_VARIANT_TYPE.value]]),           # 'tfx_variant_type', 
                         ' / '.join([x for x in vc.INFO[TranscriptEffect.TFX_AMINO_ACID_POSITION.value]]),    # 'tfx_hgvs_amino_acid_position', 
@@ -130,15 +130,15 @@ def _parse_args():
     '''
     Validate and return command line arguments.
     '''
-    parser = argparse.ArgumentParser(description='Join a VCF containing transcript effects with a heme variant export')
+    parser = argparse.ArgumentParser(description='Join a VCF containing transcript effects with a CSV from variant export')
 
     parser.add_argument('-v', '--vcf',  
                         help='Input VCF.bgz containing transcript effects (index must also exist)',
                         type=argparse.FileType('r'),
                         required=True)
 
-    parser.add_argument('-c', '--heme_csv',  
-                        help='Heme variant export',
+    parser.add_argument('-c', '--export_csv',  
+                        help='Variant export CSV (use variant_transcripts.sql to generate the csv)',
                         type=argparse.FileType('r'),
                         required=True)
         
@@ -156,7 +156,7 @@ def _main():
     main function
     '''
     args = _parse_args()
-    process(args.vcf.name, args.heme_csv.name, args.out_file.name)
+    process(args.vcf.name, args.export_csv.name, args.out_file.name)
 
 if __name__ == '__main__':
     _main()
