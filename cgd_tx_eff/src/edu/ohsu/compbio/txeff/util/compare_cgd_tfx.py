27658675360b1e365f24902dc5f8b80c2be5e13d
Jay <22547630+jPleyte@users.noreply.github.com>
2023-01-11 12:37:02 -0800

    CGD-1984 - Rename some fields and add genomicVariant.id

--- cgd_tx_eff/src/edu/ohsu/compbio/txeff/util/compare_cgd_tfx.py
+++ cgd_tx_eff/src/edu/ohsu/compbio/txeff/util/compare_cgd_tfx.py
@@ -54,9 +54,9 @@ def find_in_vcf(chromosome: str, position_start: int, position_end: int, ref: st
     return None
 
 def process(vcf_filename: str, csv_heme_filename: str, csv_out_filename: str):
-    out_fields = ['chromosome', 'position', 'reference', 'alt',
+    out_fields = ['genomic_variant_id', 'chromosome', 'position', 'reference', 'alt',
                   'cgd_variant_effect', 'cgd_variant_type', 'cgd_hgvs_amino_acid_position', 'cgd_hgvs_base_position', 
-                  'cgd_exon', 'cgd_hgnc_gene', 'cgd_hgvs_c_dot', 'cgd_hgvs_p_dot_one', 'cgd_hgvs_p_dot_three', 
+                  'cgd_exon', 'cgd_hgnc_gene', 'cgd_primary_gene', 'cgd_hgvs_c_dot', 'cgd_hgvs_p_dot_one', 'cgd_hgvs_p_dot_three', 
                   'cgd_splicing', 'cgd_refseq_transcript', 'cgd_protein_transcript',
                   'tfx_variant_effect', 'tfx_variant_type', 'tfx_hgvs_amino_acid_position', 'tfx_hgvs_base_position', 
                   'tfx_exon', 'tfx_hgnc_gene', 'tfx_hgvs_c_dot', 'tfx_hgvs_p_dot_one', 'tfx_hgvs_p_dot_three', 
@@ -96,19 +96,21 @@ def process(vcf_filename: str, csv_heme_filename: str, csv_out_filename: str):
 def write(csv_writer, heme_row, vc: vcfpy.record.Record):
     '''
     '''    
-    csv_writer.writerow([heme_row['chromosome'], heme_row['position_start'], heme_row['reference_base'], heme_row['variant_base'],
+    csv_writer.writerow([heme_row['genomic_variant_id'],
+                         heme_row['chromosome'], heme_row['position_start'], heme_row['reference_base'], heme_row['variant_base'],
                          heme_row['protein_variant_type'],                          # 'cgd_variant_effect' - annovar variant_effect is cgd's protein variant type
                          heme_row['genomic_variant_type'],                          # 'cgd_variant_type'
                          heme_row['amino_acid_position'],                           # 'cgd_hgvs_amino_acid_position' 
                          heme_row['base_pair_position'],                            # 'cgd_hgvs_base_position', 
-                         heme_row['exons'],                                         # 'cgd_exon'
-                         heme_row['gene'],                                          # 'cgd_hgnc_gene'
+                         heme_row['exon'],                                          # 'cgd_exon'
+                         heme_row['genes'],                                         # 'cgd_hgnc_gene'
+                         heme_row['primary_gene'],                                   # 'cgd_primary_gene'
                          heme_row['genotype_cdna'],                                 # 'cgd_hgvs_c_dot'
                          heme_row['genotype_amino_acid_onel'],                      # 'cgd_hgvs_p_dot_one' 
                          heme_row['genotype_amino_acid_threel'],                    # 'cgd_hgvs_p_dot_three' 
                          heme_row['splice_site_relationship'],                      # 'cgd_splicing'
-                         heme_row['transcripts'],                                   # 'cgd_refseq_transcript'
-                         heme_row['protein_transcripts'],                           # 'cgd_protein_transcript'                        
+                         heme_row['cdna_transcript'],                               # 'cgd_refseq_transcript'
+                         heme_row['protein_transcript'],                           # 'cgd_protein_transcript'
                         ' / '.join([x for x in vc.INFO[TranscriptEffect.TFX_VARIANT_EFFECT.value]]),         # 'tfx_variant_effect'
                         ' / '.join([x for x in vc.INFO[TranscriptEffect.TFX_VARIANT_TYPE.value]]),           # 'tfx_variant_type', 
                         ' / '.join([x for x in vc.INFO[TranscriptEffect.TFX_AMINO_ACID_POSITION.value]]),    # 'tfx_hgvs_amino_acid_position', 
