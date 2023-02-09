306593aa7aa027720f79e478ef8b92568bbd0f79
Jay <22547630+jPleyte@users.noreply.github.com>
2023-02-09 14:18:02 -0800

    CGD-1984 - Include stoploss in the CGD fields

--- cgd_tx_eff/src/edu/ohsu/compbio/txeff/util/compare_cgd_tfx.py
+++ cgd_tx_eff/src/edu/ohsu/compbio/txeff/util/compare_cgd_tfx.py
@@ -55,7 +55,7 @@ def find_in_vcf(chromosome: str, position_start: int, position_end: int, ref: st
 
 def process(vcf_filename: str, csv_export_filename: str, csv_out_filename: str):
     out_fields = ['genomic_variant_id', 'chromosome', 'position', 'reference', 'alt',
-                  'cgd_variant_effect', 'cgd_variant_type', 'cgd_hgvs_amino_acid_position', 'cgd_hgvs_base_position', 
+                  'cgd_variant_effect', 'cgd_variant_type', 'cgd_stop_gain_loss', 'cgd_hgvs_amino_acid_position', 'cgd_hgvs_base_position', 
                   'cgd_exon', 'cgd_hgnc_gene', 'cgd_primary_gene', 'cgd_hgvs_c_dot', 'cgd_hgvs_p_dot_one', 'cgd_hgvs_p_dot_three', 
                   'cgd_splicing', 'cgd_refseq_transcript', 'cgd_protein_transcript',
                   'tfx_variant_effect', 'tfx_variant_type', 'tfx_hgvs_amino_acid_position', 'tfx_hgvs_base_position', 
@@ -100,6 +100,7 @@ def write(csv_writer, export_row, vc: vcfpy.record.Record):
                          export_row['chromosome'], export_row['position_start'], export_row['reference_base'], export_row['variant_base'],
                          export_row['protein_variant_type'],                          # 'cgd_variant_effect' - annovar variant_effect is cgd's protein variant type
                          export_row['genomic_variant_type'],                          # 'cgd_variant_type'
+                         export_row['stop_gain_loss'],                                # 'cgd_stop_gain_loss'
                          export_row['amino_acid_position'],                           # 'cgd_hgvs_amino_acid_position' 
                          export_row['base_pair_position'],                            # 'cgd_hgvs_base_position', 
                          export_row['exon'],                                          # 'cgd_exon'
@@ -110,7 +111,7 @@ def write(csv_writer, export_row, vc: vcfpy.record.Record):
                          export_row['genotype_amino_acid_threel'],                    # 'cgd_hgvs_p_dot_three' 
                          export_row['splice_site_relationship'],                      # 'cgd_splicing'
                          export_row['cdna_transcript'],                               # 'cgd_refseq_transcript'
-                         export_row['protein_transcript'],                           # 'cgd_protein_transcript'
+                         export_row['protein_transcript'],                            # 'cgd_protein_transcript'
                         ' / '.join([x for x in vc.INFO[TranscriptEffect.TFX_VARIANT_EFFECT.value]]),         # 'tfx_variant_effect'
                         ' / '.join([x for x in vc.INFO[TranscriptEffect.TFX_VARIANT_TYPE.value]]),           # 'tfx_variant_type', 
                         ' / '.join([x for x in vc.INFO[TranscriptEffect.TFX_AMINO_ACID_POSITION.value]]),    # 'tfx_hgvs_amino_acid_position', 
