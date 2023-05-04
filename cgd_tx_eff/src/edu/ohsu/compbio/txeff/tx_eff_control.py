e255e77b7d29eb1db0387abe98d84708e345fe35
Jay <22547630+jPleyte@users.noreply.github.com>
2023-05-04 10:39:58 -0700

    BIO-630 - Formatting changes

--- cgd_tx_eff/src/edu/ohsu/compbio/txeff/tx_eff_control.py
+++ cgd_tx_eff/src/edu/ohsu/compbio/txeff/tx_eff_control.py
@@ -69,16 +69,16 @@ def _main():
 
     # Use tx_eff_hgvs to fix the nomenclature
     tx_eff_hgvs.identify_hgvs_datasources() 
-    merged_transcripts = tx_eff_hgvs.get_updated_hgvs_transcripts(annovar_records)
+    selected_transcripts = tx_eff_hgvs.get_updated_hgvs_transcripts(annovar_records)
 
     # Use tx_eff_ccds to add CCDS transcripts
     tx_eff_ccds = TxEffCcds(args.ccds_map.name)
-    tx_eff_ccds.add_ccds_transcripts(merged_transcripts)
+    tx_eff_ccds.add_ccds_transcripts(selected_transcripts)
      
     # Use tx_eff_vcf to write the transcript effects to a VCF
-    tx_eff_vcf.create_vcf_with_transcript_effects(args.in_vcf.name, args.out_vcf.name, merged_transcripts)
+    tx_eff_vcf.create_vcf_with_transcript_effects(args.in_vcf.name, args.out_vcf.name, selected_transcripts)
     
-    print(f"Wrote {len(merged_transcripts)} transcripts to {args.out_vcf.name}")
+    print(f"Wrote {len(selected_transcripts)} transcripts to {args.out_vcf.name}")
     
     end_time = time.time()
     total_time = end_time - start_time
