96a320baf9c83fd4630b4ab1dbf93ed25f396a73
Jay <22547630+jPleyte@users.noreply.github.com>
2024-07-17 11:46:27 -0700

    BIO-722 - Handle multiple refseq mapping to same ccds

---
+++ cgd_tx_eff/test/edu/ohsu/compbio/txeff/test_tx_eff_ccds.py
@@ -0,0 +1,37 @@
+import unittest
+from edu.ohsu.compbio.txeff.tx_eff_ccds import TxEffCcds
+from edu.ohsu.compbio.txeff.variant_transcript import VariantTranscript
+
+class TxEffCcdsTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.txEffCcds = TxEffCcds(None)
+
+    def test__get_preferred_refseq_transcript_different_accesion(self):
+        vt0 = VariantTranscript('1', 123, 'C', 'G')
+        vt0.refseq_transcript = 'NM_000051.4'
+
+        vt1 = VariantTranscript('1', 123, 'C', 'G')
+        vt1.refseq_transcript = 'NM_001351834.2'
+
+        preferred = self.txEffCcds._get_preferred_refseq_transcript(vt0, vt1)
+        self.assertEqual(preferred.refseq_transcript, 'NM_000051.4', "Earliest accession should be preferred")
+
+    def test__get_preferred_refseq_transcript_different_version(self):
+        vt0 = VariantTranscript('1', 123, 'C', 'G')
+        vt0.refseq_transcript = 'NM_000051.4'
+
+        vt1 = VariantTranscript('1', 123, 'C', 'G')
+        vt1.refseq_transcript = 'NM_000051.5'
+        preferred = self.txEffCcds._get_preferred_refseq_transcript(vt0, vt1)
+
+        self.assertEqual(preferred.refseq_transcript, 'NM_000051.5', "Latest version should be preferred")
+    
+    def test__get_preferred_refseq_transcript_non_refseq(self):
+        vt0 = VariantTranscript('1', 123, 'C', 'G')
+        vt0.refseq_transcript = 'NP_000000.0'
+
+        vt1 = VariantTranscript('1', 123, 'C', 'G')
+        vt1.refseq_transcript = 'NM_000051.5'
+
+        self.assertRaises(ValueError, self.txEffCcds._get_preferred_refseq_transcript, vt0, vt1)
