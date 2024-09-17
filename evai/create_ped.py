24cb9fdb570d1531e0ff2171de88a65695085ba1
chongbe <benson@machuno.com>
2024-09-17 16:49:30 -0700

    create conditions for default ubaffected parental phenotypes if none are entered and output path arg

--- evai/create_ped.py
+++ evai/create_ped.py
@@ -5,22 +5,32 @@ import argparse
 VERSION = '0.0.1'
 
 parser = argparse.ArgumentParser(description='')
+parser.add_argument('--output_ped','-o')
 parser.add_argument('family_id', help='')
 parser.add_argument('proband_id', help='')
 parser.add_argument('proband_sex', help='')
-parser.add_argument('proband_ph', help='')
 parser.add_argument('father_id', help='')
-parser.add_argument('father_ph', help='')
 parser.add_argument('mother_id', help='')
-parser.add_argument('mother_ph', help='')
+parser.add_argument('--father_ph', '-f')
+parser.add_argument('--mother_ph', '-m')
 
 args = parser.parse_args()
 
-proband = [args.family_id, args.proband_id, args.father_id, args.mother_id, args.proband_sex, args.proband_ph]
-father = [args.family_id, args.father_id, '0', '0', '1', args.father_ph]
-mother = [args.family_id, args.mother_id, '0', '0', '2', args.mother_ph]
+# Optional inputs for parental phenotypes
+if args.father_ph is None:
+    fp = '1'
+else:
+    fp = args.father_ph
+if args.mother_ph is None:
+    mp = '1'
+else:
+    mp = args.mother_ph
 
-with open('evai.ped', 'w') as f:
+proband = [args.family_id, args.proband_id, args.father_id, args.mother_id, args.proband_sex, '2']
+father = [args.family_id, args.father_id, '0', '0', '1', fp]
+mother = [args.family_id, args.mother_id, '0', '0', '2', mp]
+
+with open(args.output_ped, 'w') as f:
     f.write('\t'.join(proband) + '\n')
     f.write('\t'.join(father) + '\n')
     f.write('\t'.join(mother))
