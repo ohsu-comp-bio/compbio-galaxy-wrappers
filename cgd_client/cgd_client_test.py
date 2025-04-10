43d28f4088a9f96b5f87f56df75ff30005284c1b
Jay <22547630+jPleyte@users.noreply.github.com>
2025-04-09 17:42:29 -0700

    BIO-773 - Create global parameters

--- cgd_client/cgd_client_test.py
+++ cgd_client/cgd_client_test.py
@@ -9,16 +9,24 @@ from unittest.mock import patch
 import cgd_client
 import pathlib as pl
 
+JAVA_BIN = '/usr/bin/java'
+CGD_CLIENT_JAR = 'cgd_client-1.2.10.jar'
+CGD_CLIENT_PROPERTIES = 'cgd_client.properties'
+SERVICE_BASE = 'http://localhost:8080/cgd'
+
+# Run id and barcode id must be fore a run that has a patient 
+RUN_ID = ''
+BARCODE_ID = ''
 
 class CgdClientTest(unittest.TestCase):
 
     @patch('sys.argv', ['cgd_client.py', 
                         '--endpoint', 'requestVariants', 
                         '--json_out', '/tmp/variants.json', 
-                        '--java8_path', '/Library/Java/JavaVirtualMachines/openjdk-8.jdk/Contents/Home/bin/java', 
-                        '--cgd_client', '/Users/pleyte/.m2/repository/edu/ohsu/kdl/cgd_client/1.2.10-SNAPSHOT/cgd_client-1.2.10-SNAPSHOT.jar', 
-                        '--cgd_config', '/Users/pleyte/Documents/office/ohsu/cgd_client/cgd_client.properties', 
-                        '--servicebase', 'https://kdlcgdwebdev2.ohsu.edu/tfx_cgd/', 
+                        '--java8_path', JAVA_BIN, 
+                        '--cgd_client', CGD_CLIENT_JAR, 
+                        '--cgd_config', CGD_CLIENT_PROPERTIES, 
+                        '--servicebase', SERVICE_BASE, 
                         '--pipeline_out', 'test-data/variant_request.json'])
     def test_main_endpoint_variantRequest(self):
         cgd_client.main()
@@ -27,22 +35,18 @@ class CgdClientTest(unittest.TestCase):
 
     @patch('sys.argv', ['cgd_client.py', 
                         '--endpoint', 'reportedvariants', 
-                        '--java8_path', '/Library/Java/JavaVirtualMachines/openjdk-8.jdk/Contents/Home/bin/java', 
-                        '--cgd_client', '/Users/pleyte/.m2/repository/edu/ohsu/kdl/cgd_client/1.2.10-SNAPSHOT/cgd_client-1.2.10-SNAPSHOT.jar', 
-                        '--cgd_config', '/Users/pleyte/Documents/office/ohsu/cgd_client/cgd_client.properties', 
-                        '--servicebase', 'https://kdlcgdwebdev2.ohsu.edu/tfx_cgd/',
-                        '--runid', '231121_NS500390_0123_AHG7WKBGXV',
-                        '--barcodeid', 'E02', 
+                        '--java8_path', JAVA_BIN, 
+                        '--cgd_client', CGD_CLIENT_JAR, 
+                        '--cgd_config', CGD_CLIENT_PROPERTIES, 
+                        '--servicebase', SERVICE_BASE,
+                        '--runid', RUN_ID,
+                        '--barcodeid', BARCODE_ID, 
                         '--report_vcf', '/tmp/reported_variants.vcf',
                         '--report_bed', '/tmp/reported_variants.bed'])
     def test_main_endpoint_reportedvariants(self):
         cgd_client.main()
         self.assertTrue(pl.Path("/tmp/reported_variants.vcf").is_file(), "reported variants vcf")
         self.assertTrue(pl.Path("/tmp/reported_variants.bed").is_file(), "reported variants bed")
-    
-    def test_main_endpoint_snpProfile(self):
-        cgd_client.main()
             
 if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
